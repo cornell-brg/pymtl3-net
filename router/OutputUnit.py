1389da5e4d770b8701707de436638706e9f5c55a
yo96 <yo96@cornell.edu>
2019-02-11 00:30:00 -0500

    OutputUnit: becomes straight wires when num_entries==0.

--- router/OutputUnit.py
+++ router/OutputUnit.py
@@ -13,11 +13,14 @@ class OutputUnit( Model ):
     s.out = OutValRdyBundle( msg_type )
 
     # Component
-    s.queue = NormalQueue( num_entries, msg_type ) 
+    if num_entries > 0:
+      s.queue = NormalQueue( num_entries, msg_type ) 
+      s.connect( s.in_, s.queue.enq )
+      s.connect( s.out, s.queue.deq )
     
-    # Connections
-    s.connect( s.in_, s.queue.enq )
-    s.connect( s.out, s.queue.deq )
+    # No ouput queue
+    else:
+      s.connect( s.in_, s.out ) 
 
   def line_trace( s ):
     return "{} || {}".format( s.in_, s.out )
