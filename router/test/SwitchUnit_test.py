e43f245700ab7684ba26d8bbdb5e177226af0037
tancheng <tancheng1990@gmail.com>
2019-02-12 01:23:35 -0500

    initial test for SwitchUnit.py

--- router/test/SwitchUnit_test.py
+++ router/test/SwitchUnit_test.py
@@ -1,41 +1,53 @@
 import tempfile
 from pymtl                import *
-from pclib.test           import TestVectorSimulator
-from pc.GrantHoldArbiter import GrantHoldArbiter
-from hypothesis.stateful import * 
-from hypothesis import strategies as st
+from pclib.test           import TestVectorSimulator, TestSource, TestSink
+from router.SwitchUnit import SwitchUnit
 
 def run_test( model, test_vectors ):
  
   model.elaborate()
 
-  def tv_in( dut, test_vector ):
-    dut.reqs.value = test_vector[0]
-    dut.reqs.value = test_vector[0]
-    dut.reqs.value = test_vector[0]
+  def tv_in( model, test_vector ):
+    model.out.rdy.value = test_vector[2]
+    for i in range( model.num_inports ):
+      if i == test_vector[0]:
+        model.in_[i].val.value = 1
+        model.in_[i].msg.value = test_vector[1]
+      else:
+        model.in_[i].val.value = 0
+        model.in_[i].msg.value = 0
 
   def tv_out( model, test_vector ):
-    assert model.grants == test_vector[2]
+    if test_vector[4] != '?':
+      assert model.out.val == test_vector[3]
+      if model.out.val == 1:      
+        assert model.out.msg == test_vector[4]
   
   sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
   sim.run_test()
 
-def test_arb_4_directed( dump_vcd, test_verilog ):
-  model = GrantHoldArbiter( 4 )
-  model.vcd_file = dump_vcd
-  if test_verilog:
-    model = TranslationTool( model )
+def test_SwitchUnit( dump_vcd, test_verilog ):
+  model = SwitchUnit( 16, 5 )
 
   run_test( model, [
-    # reqs    hold grants      priority
-    [ 0b0000, 0,   0b0000 ], # 0001
-    [ 0b1000, 0,   0b1000 ], # 0001
-    [ 0b1100, 0,   0b0100 ], # 0100  
-    [ 0b1100, 0,   0b1000 ], # 1000
-    [ 0b0000, 1,   0b1000 ], # 1000
-    [ 0b0111, 0,   0b0001 ], # 1000
-    [ 0b1111, 0,   0b0010 ], # 0010
-    [ 0b1111, 1,   0b0010 ], # 0010
-    [ 0b1111, 1,   0b0010 ], # 0010
-  ] )
+    # val_index   msg      out_rdy    out_val    out_msg 
+   [     1,        7,         0,         1,         '?'   ],
+   [     4,        9,         0,         1,          9    ],
+   [     2,        5,         0,         1,          5    ],
+   [     3,        2,         1,         1,          2    ],
+   [     0,        4,         1,         1,          4    ],
+  ])
+
+#  run_test( model, [
+#    # reqs    hold grants      priority
+#    [ 0b0000, 0,   0b0000 ], # 0001
+#    [ 0b1000, 0,   0b1000 ], # 0001
+#    [ 0b1100, 0,   0b0100 ], # 0100  
+#    [ 0b1100, 0,   0b1000 ], # 1000
+#    [ 0b0000, 1,   0b1000 ], # 1000
+#    [ 0b0111, 0,   0b0001 ], # 1000
+#    [ 0b1111, 0,   0b0010 ], # 0010
+#    [ 0b1111, 1,   0b0010 ], # 0010
+#    [ 0b1111, 1,   0b0010 ], # 0010
+#  ] )
 
