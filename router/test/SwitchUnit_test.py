1feba5b0818b0c0a683129cdf3ef86953f388683
tancheng <tancheng1990@gmail.com>
2019-02-12 12:09:25 -0500

    test for SwitchUnit.py

--- router/test/SwitchUnit_test.py
+++ router/test/SwitchUnit_test.py
@@ -10,12 +10,8 @@ def run_test( model, test_vectors ):
   def tv_in( model, test_vector ):
     model.out.rdy.value = test_vector[2]
     for i in range( model.num_inports ):
-      if i == test_vector[0]:
-        model.in_[i].val.value = 1
-        model.in_[i].msg.value = test_vector[1]
-      else:
-        model.in_[i].val.value = 0
-        model.in_[i].msg.value = 0
+      model.in_[i].val.value = test_vector[0][i]
+      model.in_[i].msg.value = test_vector[1][i]
 
   def tv_out( model, test_vector ):
     if test_vector[4] != '?':
@@ -30,24 +26,32 @@ def test_SwitchUnit( dump_vcd, test_verilog ):
   model = SwitchUnit( 16, 5 )
 
   run_test( model, [
-    # val_index   msg      out_rdy    out_val    out_msg 
-   [     1,        7,         0,         1,         '?'   ],
-   [     4,        9,         0,         1,          9    ],
-   [     2,        5,         0,         1,          5    ],
-   [     3,        2,         1,         1,          2    ],
-   [     0,        4,         1,         1,          4    ],
+    # val            msg      out_rdy    out_val    out_msg 
+   [[0,0,0,0,0], [5,6,7,8,9],    0,         0,         '?'   ],
+   [[0,1,0,0,0], [1,2,3,4,5],    1,         1,          2    ],
+   [[0,1,1,1,0], [9,8,7,6,5],    1,         1,          7    ],
+   [[0,1,1,1,0], [5,4,3,2,1],    1,         1,          2    ],
+   [[1,0,0,0,1], [3,4,5,6,7],    1,         1,          7    ],
+   [[0,1,1,0,1], [3,4,5,6,7],    1,         1,          4    ],
   ])
 
 #  run_test( model, [
-#    # reqs    hold grants      priority
-#    [ 0b0000, 0,   0b0000 ], # 0001
-#    [ 0b1000, 0,   0b1000 ], # 0001
-#    [ 0b1100, 0,   0b0100 ], # 0100  
-#    [ 0b1100, 0,   0b1000 ], # 1000
-#    [ 0b0000, 1,   0b1000 ], # 1000
-#    [ 0b0111, 0,   0b0001 ], # 1000
-#    [ 0b1111, 0,   0b0010 ], # 0010
-#    [ 0b1111, 1,   0b0010 ], # 0010
-#    [ 0b1111, 1,   0b0010 ], # 0010
-#  ] )
+#    # val_index   msg      out_rdy    out_val    out_msg 
+#   [     1,        7,         0,         1,         '?'   ],
+#   [     4,        9,         0,         1,          9    ],
+#   [     2,        5,         0,         1,          5    ],
+#   [     3,        2,         1,         1,          2    ],
+#   [     0,        4,         1,         1,          4    ],
+#  ])
+
+
+
+
+
+
+
+
+
+
+
 
