5d572e7199d698fd593aec82503bca2520e84e7b
yo96 <yo96@cornell.edu>
2019-02-19 12:36:00 -0500

    Added ocn_pclib and implemented a test vector simulator.

--- router/test/InputUnitRTL_test.py
+++ router/test/InputUnitRTL_test.py
@@ -8,38 +8,7 @@
 
 from pymtl import *
 from router.InputUnitRTL import InputUnitRTL
-from pymtl.passes.PassGroups import SimpleSim
-# from pclib.rtl.valrdy_queues_test import TestVectorSimulator
-
-class TestVectorSimulator( object ):
-
-  def __init__( self, model, test_vectors,
-                set_inputs_func, verify_outputs_func, wait_cycles = 0 ):
-
-    self.model               = model
-    self.set_inputs_func     = set_inputs_func
-    self.verify_outputs_func = verify_outputs_func
-    self.test_vectors        = test_vectors
-    self.wait_cycles         = wait_cycles
-
-  def run_test( self ):
-
-    # self.model.elaborate()
-    self.model.apply( SimpleSim )
-
-    print()
-    for test_vector in self.test_vectors:
-
-      # Set inputs
-      self.set_inputs_func( self.model, test_vector )
-      self.model.tick()
-
-      # Print the line trace
-      print self.model.line_trace()
-
-      # Verify outputs
-      self.verify_outputs_func( self.model, test_vector )
-      
+from ocn_pclib.TestVectorSimulator import TestVectorSimulator
 
 #-------------------------------------------------------------------------
 # run_test_queue
