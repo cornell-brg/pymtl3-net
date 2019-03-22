b0d27274bd235e32739e5b2759e18ebb813476bb
yo96 <yo96@cornell.edu>
2019-03-21 21:44:16 -0400

    re-arranged project hierachy.

---
+++ network/TorusNetworkRTL.py
@@ -0,0 +1,124 @@
+#=========================================================================
+# NetworkRTL.py
+#=========================================================================
+# Simple network implementation.
+#
+# Author : Cheng Tan
+#   Date : Mar 10, 2019
+
+from pymtl                       import *
+from pclib.ifcs.EnRdyIfc         import InEnRdyIfc, OutEnRdyIfc
+from ocn_pclib.Packet            import Packet
+from ocn_pclib.Position          import *
+
+from src.router.RouterRTL        import RouterRTL
+from src.router.InputUnitRTL     import InputUnitRTL
+from src.router.DORXRouteUnitRTL import DORXRouteUnitRTL
+from src.router.DORYRouteUnitRTL import DORYRouteUnitRTL
+from src.router.SwitchUnitRTL    import SwitchUnitRTL
+from src.router.OutputUnitRTL    import OutputUnitRTL
+from src.ChannelUnitRTL          import ChannelUnitRTL
+
+from Configs import configure_network
+
+class TorusNetworkRTL( RTLComponent ):
+#  def construct( s, RouterType, RoutingStrategyType, TopologyType ):
+  def construct( s ):
+
+    # Constants
+    configs = configure_network()
+
+    NORTH = 0
+    SOUTH = 1
+    WEST  = 2
+    EAST  = 3
+    SELF  = 4
+
+    s.num_inports     = configs.router_inports
+    s.num_outports    = configs.router_outports
+    s.num_routers     = configs.routers
+    s.channel_latency = 0
+    s.rows     = configs.rows
+    s.cols     = s.num_routers/s.rows
+
+    # Type for Mesh Network RTL
+    s.PacketType     = Packet
+    s.PositionType   = MeshPosition
+    s.InputUnitType  = InputUnitRTL
+    s.RouteUnitType  = DORYRouteUnitRTL
+    s.SwitchUnitType = SwitchUnitRTL
+    s.OutputUnitType = OutputUnitRTL
+
+    # Interface
+    s.recv = [InEnRdyIfc(s.PacketType)  for _ in range(s.num_routers*4)]
+    s.send = [OutEnRdyIfc(s.PacketType) for _ in range(s.num_routers*4)]
+    s.recv_noc_ifc = [InEnRdyIfc(s.PacketType)  for _ in range(s.num_routers)]
+    s.send_noc_ifc = [OutEnRdyIfc(s.PacketType) for _ in range(s.num_routers)]
+
+    # This outputs used to print the direction of the routing
+    s.outputs = [ Wire( Bits3 )  for _ in range(s.num_routers*s.num_inports)]
+    s.pos_ports = [ InVPort( s.PositionType ) for _ in range(s.num_routers)]
+
+    # Components
+
+    s.routers = [RouterRTL(s.PacketType, s.PositionType, s.num_inports,
+        s.num_outports, s.InputUnitType, s.RouteUnitType, s.SwitchUnitType,
+        s.OutputUnitType) for i in range(s.num_routers)]
+
+    num_channels = 4*s.rows*s.cols
+
+    s.channels   = [ChannelUnitRTL(PacketType=s.PacketType, latency=s.channel_latency)
+            for _ in range(num_channels) ]
+
+    for i in range( s.num_routers ):
+      for j in range( s.num_inports):
+        s.connect( s.outputs[i*s.num_inports+j],  s.routers[i].outs[j] )
+      s.connect( s.pos_ports[i], s.routers[i].pos )
+
+    channel_index  = 0
+    # recv/send_index
+    rs_i = s.num_routers
+
+    for i in range (s.num_routers):
+      # Connect s.routers together in Mesh
+      s.connect(s.routers[i].send[NORTH], s.channels[channel_index].recv)
+      s.connect(s.channels[channel_index].send, s.routers[(i-s.rows+
+          s.num_routers)%s.num_routers].recv[SOUTH])
+      channel_index += 1
+ 
+      s.connect(s.routers[i].send[SOUTH], s.channels[channel_index].recv)
+      s.connect(s.channels[channel_index].send, s.routers[(i+s.rows+
+          s.num_routers)%s.num_routers].recv[NORTH])
+      channel_index += 1
+ 
+      s.connect(s.routers[i].send[WEST],  s.channels[channel_index].recv)
+      s.connect(s.channels[channel_index].send, s.routers[(i-1+
+           s.num_routers)%s.num_routers].recv[EAST])
+      channel_index += 1
+ 
+      s.connect(s.routers[i].send[EAST],  s.channels[channel_index].recv)
+      s.connect(s.channels[channel_index].send, s.routers[(i+1+
+          s.num_routers)%s.num_routers].recv[WEST])
+      channel_index += 1
+
+      # Connect the self port (with Network Interface)
+      s.connect(s.recv_noc_ifc[i], s.routers[i].recv[SELF])
+      s.connect(s.send_noc_ifc[i], s.routers[i].send[SELF])
+
+  def line_trace( s ):
+    trace = ''
+    for r in range(s.num_routers):
+      trace += '\n({},{})|'.format(s.pos_ports[r].pos_x, s.pos_ports[r].pos_y)
+      for i in range(s.num_inports):
+        trace += '|{}:{}->({},{})'.format( i, 
+                s.routers[r].recv[i].msg.payload, 
+                s.routers[r].recv[i].msg.dst_x,
+                s.routers[r].recv[i].msg.dst_y)
+      trace += '\n out: '
+      for i in range(s.num_outports):
+        trace += '|{}:{}->({},{})'.format( i, 
+                s.routers[r].send[i].msg.payload, 
+                s.routers[r].send[i].msg.dst_x,
+                s.routers[r].send[i].msg.dst_y)
+    return trace
+    
---
+++ router/test/DORYRouteUnitRTL_test.py
@@ -0,0 +1,62 @@
+#=========================================================================
+# DORYRouteUnitRTL_test.py
+#=========================================================================
+# Test for DORYRouteUnitRTL
+#
+# Author : Cheng Tan, Yanghui Ou
+#   Date : Mar 3, 2019
+
+import tempfile
+from pymtl                import *
+from ocn_pclib.TestVectorSimulator            import TestVectorSimulator
+from ocn_pclib.Packet import Packet, mk_pkt
+from src.router.DORYRouteUnitRTL  import DORYRouteUnitRTL
+
+from ocn_pclib.Position import *
+
+from Configs import configure_network
+
+def run_test( model, test_vectors ):
+ 
+  def tv_in( model, test_vector ):
+
+    # assume a 2x2 Mesh network
+    pos = MeshPosition( 3, 1, 1)
+    model.pos = pos
+
+    pkt = mk_pkt(0, 0, test_vector[0], test_vector[1], test_vector[2], test_vector[3])
+    model.recv.msg = pkt
+
+    for i in range( model.num_outports ):
+      model.send[i].rdy = test_vector[4][i]
+      if test_vector[4][test_vector[5]] == 1:
+        model.recv.en = 1
+
+
+  def tv_out( model, test_vector ):
+    assert model.recv.rdy == test_vector[4][test_vector[5]]
+    if test_vector[4][test_vector[5]] == 1:
+      assert model.send[test_vector[5]].en == 1
+      assert model.send[test_vector[5]].msg.payload == test_vector[3]
+  
+  sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
+  sim.run_test()
+  model.sim_reset()
+
+def test_RouteUnit( dump_vcd, test_verilog ):
+
+  configs = configure_network()
+
+  model = DORYRouteUnitRTL( Packet, MeshPosition )
+
+  model.set_parameter("top.elaborate.num_outports", 5)
+
+  # specific for DOR Y routing algorithm
+  run_test( model, [
+    #  dst_x  dst_y  opaque  payload      rdy       dir
+   [     1,     1,     1,       9,    [0,0,0,0,1],   4  ],
+   [     2,     2,     1,       7,    [1,0,0,0,0],   1  ],
+   [     2,     1,     1,       3,    [0,0,1,0,0],   3  ],
+   [     1,     2,     1,       6,    [0,1,0,1,0],   1  ],
+   [     0,     0,     1,       4,    [1,0,0,0,0],   0  ],
+  ])
