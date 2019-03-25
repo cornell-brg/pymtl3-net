051f2c529386a7d1cbfa7d2fbcd654054980f229
yo96 <yo96@cornell.edu>
2019-03-24 22:54:08 -0400

    added src/sink test. not working because of SimpleSim is not compatible with ComponentLevel6

--- ocn_pclib/rtl/queues_test.py
+++ ocn_pclib/rtl/queues_test.py
@@ -59,74 +59,74 @@ def test_pipe_Bits():
 # TestHarness
 #-------------------------------------------------------------------------
 
-# class TestHarness( ComponentLevel6 ):
-# 
-#   def construct( s, MsgType, qsize, src_msgs, sink_msgs, src_initial, 
-#                  src_interval, sink_initial, sink_interval, 
-#                  arrival_time=None ):
-# 
-#     s.src     = TestSrcRTL ( MsgType, src_msgs,  src_initial,  src_interval  )
-#     s.dut     = NormalQueueRTL( MsgType, qsize )
-#     s.sink    = TestSinkRTL( MsgType, sink_msgs, sink_initial, sink_interval, 
-#                              arrival_time )
-#     
-#     s.connect( s.src.send,    s.dut.enq       )
-#     s.connect( s.dut.deq.msg, s.sink.recv.msg )
-# 
-#     @s.update
-#     def up_deq_send():
-#       if s.dut.deq.rdy and s.sink.recv.rdy:
-#         s.dut.deq.en   = Bits1( 1 )
-#         s.sink.recv.en = Bits1( 1 )
-#       else:
-#         s.dut.deq.en   = Bits1( 0 )
-#         s.sink.recv.en = Bits1( 0 )
-# 
-#   def done( s ):
-#     return s.src.done() and s.sink.done()
-# 
-#   def line_trace( s ):
-#     return "{} ({}) {}".format( 
-#       s.src.line_trace(), s.dut.line_trace(), s.sink.line_trace() )
+class TestHarness( ComponentLevel6 ):
+
+  def construct( s, MsgType, qsize, src_msgs, sink_msgs, src_initial, 
+                 src_interval, sink_initial, sink_interval, 
+                 arrival_time=None ):
+
+    s.src     = TestSrcRTL ( MsgType, src_msgs,  src_initial,  src_interval  )
+    s.dut     = NormalQueueRTL( MsgType, qsize )
+    s.sink    = TestSinkRTL( MsgType, sink_msgs, sink_initial, sink_interval, 
+                             arrival_time )
+    
+    s.connect( s.src.send,    s.dut.enq       )
+    s.connect( s.dut.deq.msg, s.sink.recv.msg )
+
+    @s.update
+    def up_deq_send():
+      if s.dut.deq.rdy and s.sink.recv.rdy:
+        s.dut.deq.en   = Bits1( 1 )
+        s.sink.recv.en = Bits1( 1 )
+      else:
+        s.dut.deq.en   = Bits1( 0 )
+        s.sink.recv.en = Bits1( 0 )
+
+  def done( s ):
+    return s.src.done() and s.sink.done()
+
+  def line_trace( s ):
+    return "{} ({}) {}".format( 
+      s.src.line_trace(), s.dut.line_trace(), s.sink.line_trace() )
 
 #-------------------------------------------------------------------------
 # run_sim 
 #-------------------------------------------------------------------------
 
-# def run_sim( th, max_cycles=100 ):
-# 
-#   # Create a simulator
-# 
-#   th.elaborate()
-#   th.apply( simple_sim_pass )
-#   # th.apply( SimpleSim )
-#   # th.sim_reset()
-#   # Run simulation
-# 
-#   print ""
-#   ncycles = 0
-#   print "{:2}:{}".format( ncycles, th.line_trace() )
-#   while not th.done() and ncycles < max_cycles:
-#     th.tick()
-#     ncycles += 1
-#     print "{:2}:{}".format( ncycles, th.line_trace() )
-#   
-#   # Check timeout
-# 
-#   assert ncycles < max_cycles
-# 
-#   th.tick()
-#   th.tick()
-#   th.tick()
+def run_sim( th, max_cycles=100 ):
+
+  # Create a simulator
+  print th
+  th.elaborate()
+  th.apply( simple_sim_pass )
+  # th.apply( SimpleSim )
+  # th.sim_reset()
+  # Run simulation
+
+  print ""
+  ncycles = 0
+  print "{:2}:{}".format( ncycles, th.line_trace() )
+  while not th.done() and ncycles < max_cycles:
+    th.tick()
+    ncycles += 1
+    print "{:2}:{}".format( ncycles, th.line_trace() )
+  
+  # Check timeout
+
+  assert ncycles < max_cycles
+
+  th.tick()
+  th.tick()
+  th.tick()
 
 #-------------------------------------------------------------------------
 # Test cases
 #-------------------------------------------------------------------------
 
-# test_msgs = [ Bits16( 0 ), Bits16( 1 ), Bits16( 2 ), Bits16( 3 ) ]
-# 
-# arrival_pipe   = [ 2, 3, 4, 5 ]
-# 
+test_msgs = [ Bits16( 0 ), Bits16( 1 ), Bits16( 2 ), Bits16( 3 ) ]
+
+arrival_pipe   = [ 2, 3, 4, 5 ]
+
 # def test_normal2_simple():
 #   th = TestHarness( Bits16, 2, test_msgs, test_msgs, 0, 0, 0, 0,
 #                     arrival_pipe )
