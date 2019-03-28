6c88e72880ee91b25b9c10c5c1431dcf9dd3c796
tancheng <tancheng1990@gmail.com>
2019-03-28 12:44:20 -0400

    implemented src/sink test for ChannelUnit

--- channel/test/ChannelUnitRTL_test.py
+++ channel/test/ChannelUnitRTL_test.py
@@ -9,7 +9,9 @@
 import pytest
 
 from pymtl                    import *
-from pymtl.dsl.test.sim_utils import simple_sim_pass
+from pclib.test.test_srcs    import TestSrcRTL
+from pclib.test.test_sinks   import TestSinkRTL
+from pymtl.passes.PassGroups import SimpleSim
 
 from ocn_pclib.rtl.queues     import NormalQueueRTL
 
@@ -76,3 +78,71 @@ def test_pipe_Bits():
 #  run_tv_test( ChannelUnitRTL( Bits32, latency=0 ), test_vector_0 )
 #  run_tv_test( ChannelUnitRTL( Bits32, NormalQueueRTL, latency=1 ), test_vector_1 )
   run_tv_test( ChannelUnitRTL( Bits32, NormalQueueRTL, latency=2 ), test_vector_2 )
+
+#-------------------------------------------------------------------------
+# TestHarness
+#-------------------------------------------------------------------------
+
+class TestHarness( ComponentLevel6 ):
+
+  def construct( s, MsgType, src_msgs, sink_msgs, src_initial,
+                 src_interval, sink_initial, sink_interval,
+                 arrival_time=None ):
+
+    s.src  = TestSrcRTL  ( MsgType, src_msgs,  src_initial,  src_interval  )
+    s.sink = TestSinkRTL ( MsgType, sink_msgs, sink_initial, sink_interval )
+    s.dut  = ChannelUnitRTL ( MsgType )
+
+    # Connections
+    s.connect( s.src.send, s.dut.recv  )
+    s.connect( s.dut.send, s.sink.recv )
+
+  def done( s ):
+    return s.src.done() and s.sink.done()
+
+  def line_trace( s ):
+    return s.src.line_trace() + "-> | " + s.dut.line_trace() + \
+                               " | -> " + s.sink.line_trace()
+
+#-------------------------------------------------------------------------
+# run_rtl_sim
+#-------------------------------------------------------------------------
+
+def run_sim( test_harness, max_cycles=100 ):
+
+  # Create a simulator
+
+  test_harness.apply( SimpleSim )
+  test_harness.sim_reset()
+
+
+  # Run simulation
+
+  ncycles = 0
+  print ""
+  print "{}:{}".format( ncycles, test_harness.line_trace() )
+  while not test_harness.done() and ncycles < max_cycles:
+    test_harness.tick()
+    ncycles += 1
+    print "{}:{}".format( ncycles, test_harness.line_trace() )
+
+  # Check timeout
+
+  assert ncycles < max_cycles
+
+  test_harness.tick()
+  test_harness.tick()
+  test_harness.tick()
+
+#-------------------------------------------------------------------------
+# Test cases
+#-------------------------------------------------------------------------
+
+test_msgs = [ Bits16( 4 ), Bits16( 1 ), Bits16( 2 ), Bits16( 3 ) ]
+
+arrival_pipe   = [ 2, 3, 4, 5 ]
+
+def test_normal2_simple():
+  th = TestHarness( Bits16, test_msgs, test_msgs, 0, 0, 0, 0,
+                    arrival_pipe )
+  run_sim( th )
