1e5239b4e21918812d062275208d20034c13dd25
tancheng <tancheng1990@gmail.com>
2019-05-19 10:03:58 -0400

    Implementing test for MeshRouterFL

--- meshnet/MeshRouterFL.py
+++ meshnet/MeshRouterFL.py
@@ -6,9 +6,10 @@
 # Author : Cheng Tan
 #   Date : May 18, 2019
 
-from pymtl                import *
-from pclib.ifcs           import RecvIfcFL
-from pclib.ifcs           import SendIfcFL
+from pymtl      import *
+from directions import *
+from pclib.ifcs import RecvIfcRTL
+from pclib.ifcs import SendIfcRTL
 
 #TODO: should inherit from Component?
 class MeshRouterFL( Component ):
@@ -22,8 +23,8 @@ class MeshRouterFL( Component ):
     # Interface
 
     s.pos = InPort( PositionType )
-    s.recv = [ RecvIfcFL( PacketType ) for _ in range( s.num_inports  ) ]
-    s.send = [ SendIfcFL( PacketType ) for _ in range( s.num_outports ) ]
+    s.recv = [ RecvIfcRTL( PacketType ) for _ in range( s.num_inports  ) ]
+    s.send = [ SendIfcRTL( PacketType ) for _ in range( s.num_outports ) ]
 
     # Components
 
@@ -31,36 +32,59 @@ class MeshRouterFL( Component ):
     s.next_arbiter = 0
 
     # Routing Logic
-    @s.tick_fl
+    @s.update
     def up_routing():
 
       for i in range( s.num_inports ):
         if s.recv[i].msg != None:
-          s.recv_queue[i].append( s.recv[i].msg )
+          s.recv_queue[i].append( s.recv[i] )
 
       current_arbiter = s.next_arbiter
       occupied_dir = []
       for _ in range( s.num_inports ):
         out_dir = 0
         index = current_arbiter % s.num_inports
-        if RouteUnitType = 'DORY':
+
+        if RouteUnitType == 'DORY':
           if len( s.recv_queue[ index ] ) != 0:
-            if s.pos.pos_x == s.recv_queue[ index ].msg.dst_x 
-             and s.pos.pos_y == s.recv_queue[ index ].msg.dst_y:
+            if s.pos.pos_x == s.recv_queue[ index ][ 0 ].msg.dst_x \
+             and s.pos.pos_y == s.recv_queue[ index ][ 0 ].msg.dst_y:
               out_dir = SELF
-            elif s.recv_queue[ index ].msg.dst_y < s.pos.pos_y:
+            elif s.recv_queue[ index ][ 0 ].msg.dst_y < s.pos.pos_y:
               out_dir = SOUTH
-            elif s.recv_queue[ index ].msg.dst_y > s.pos.pos_y:
+            elif s.recv_queue[ index ][ 0 ].msg.dst_y > s.pos.pos_y:
               out_dir = NORTH
-            elif s.recv_queue[ index ].msg.dst_x < s.pos.pos_x:
+            elif s.recv_queue[ index ][ 0 ].msg.dst_x < s.pos.pos_x:
               out_dir = WEST
             else:
               out_dir = EAST
+
             if out_dir not in occupied_dir:
               s.send[ out_dir ].msg = s.recv_queue[ index ].pop( 0 )
               occupied_dir.append( out_dir )
             else:
               s.next_arbiter = current_arbiter
+
+        if RouteUnitType == 'DORX':
+          if len( s.recv_queue[ index ] ) != 0:
+            if s.pos.pos_x == s.recv_queue[ index ][ 0 ].msg.dst_x \
+             and s.pos.pos_y == s.recv_queue[ index ][ 0 ].msg.dst_y:
+              out_dir = SELF
+            elif s.recv_queue[ index ][ 0 ].msg.dst_x < s.pos.pos_x:
+              out_dir = WEST
+            elif s.recv_queue[ index ][ 0 ].msg.dst_x > s.pos.pos_x:
+              out_dir = EAST
+            elif s.recv_queue[ index ][ 0 ].msg.dst_y < s.pos.pos_y:
+              out_dir = SOUTH
+            else:
+              out_dir = NORTH
+
+            if out_dir not in occupied_dir:
+              s.send[ out_dir ].msg = s.recv_queue[ index ].pop( 0 )
+              occupied_dir.append( out_dir )
+            else:
+              s.next_arbiter = current_arbiter
+
         current_arbiter += 1
       
   def line_trace( s ):
