806d3b4ef5330a588bb1efa114ec476b7ed4a004
yo96 <yo96@cornell.edu>
2019-05-21 16:14:15 -0400

    cl mesh, broken due to dangling port.

---
+++ meshnet/MeshNetworkCL.py
@@ -0,0 +1,97 @@
+#=========================================================================
+# MeshNetworkCL.py
+#=========================================================================
+# Mesh network cycle level implementation.
+#
+# Author : Yanghui Ou
+#   Date : May 21, 2019
+
+from pymtl                 import *
+from pclib.ifcs.GuardedIfc import GuardedCalleeIfc, GuardedCallerIfc
+from directions            import *
+from channel.ChannelCL     import ChannelCL
+from MeshRouterCL          import MeshRouterCL
+
+class MeshNetworkCL( Component ):
+  def construct( s, PacketType, PositionType, 
+                 mesh_wid = 4, mesh_ht = 4, chl_lat = 0 ):
+
+    # Constants
+
+    s.num_routers = mesh_wid * mesh_ht
+    s.num_terminals = s.num_routers
+    num_channels  = (mesh_ht*(mesh_wid-1)+mesh_wid*(mesh_ht-1)) * 2
+    chl_lat       =  0
+
+    # Interface
+
+    s.recv = [ GuardedCalleeIfc() for _ in range( s.num_terminals ) ]
+    s.send = [ GuardedCallerIfc() for _ in range( s.num_terminals ) ]
+
+    # Components
+
+    s.routers  = [ MeshRouterCL( PacketType, PositionType )
+                 for i in range( s.num_routers ) ]
+
+    s.channels = [ ChannelCL( PacketType, latency = chl_lat)
+                 for _ in range( num_channels ) ]
+
+    # Connect s.routers together in Mesh
+
+    chl_id  = 0
+    for i in range( s.num_routers ):
+      if i / mesh_wid > 0:
+        s.connect( s.routers[i].send[SOUTH], s.channels[chl_id].recv )
+        s.connect( s.channels[chl_id].send, s.routers[i-mesh_wid].recv[NORTH] )
+        chl_id += 1
+
+      if i / mesh_wid < mesh_ht - 1:
+        s.connect( s.routers[i].send[NORTH], s.channels[chl_id].recv )
+        s.connect( s.channels[chl_id].send, s.routers[i+mesh_wid].recv[SOUTH] )
+        chl_id += 1
+
+      if i % mesh_wid > 0:
+        s.connect( s.routers[i].send[WEST], s.channels[chl_id].recv )
+        s.connect( s.channels[chl_id].send, s.routers[i-1].recv[EAST] )
+        chl_id += 1
+
+      if i % mesh_wid < mesh_wid - 1:
+        s.connect( s.routers[i].send[EAST], s.channels[chl_id].recv )
+        s.connect( s.channels[chl_id].send, s.routers[i+1].recv[WEST] )
+        chl_id += 1
+
+      # Connect the self port (with Network Interface)
+
+      s.connect(s.recv[i], s.routers[i].recv[SELF])
+      s.connect(s.send[i], s.routers[i].send[SELF])
+
+      # Connect the unused ports
+      def dummy_rdy():
+        return False
+      # FIXME: this doesn't work!
+      if i / mesh_wid == 0:
+        print i, SOUTH
+        s.routers[i].send[SOUTH].rdy.method = dummy_rdy
+
+      if i / mesh_wid == mesh_ht - 1:
+        print i, NORTH
+        s.routers[i].send[NORTH].rdy.method = dummy_rdy
+
+      if i % mesh_wid == 0:
+        print i, WEST
+        s.routers[i].send[WEST].rdy.method = dummy_rdy
+
+      if i % mesh_wid == mesh_wid - 1:
+        print i, EAST
+        s.routers[i].send[EAST].rdy.method = dummy_rdy
+
+    # FIXME: unable to connect a struct to a port.
+    @s.update
+    def up_pos():
+      for y in range( mesh_ht ):
+        for x in range( mesh_wid ):
+          idx = y * mesh_wid + x
+          s.routers[idx].pos = PositionType( x, y )
+
+  def line_trace( s ):
+    return "|".join( [ s.routers[i].line_trace() for i in range(s.num_routers) ] )
---
+++ meshnet/test/MeshNetworkCL_test.py
@@ -0,0 +1,156 @@
+#=========================================================================
+# MeshNetworkCL_test.py
+#=========================================================================
+# Test for NetworkCL
+#
+# Author : Yanghui Ou
+#   Date : May 19, 2019
+
+import pytest
+from pymtl                    import *
+from pclib.test               import mk_test_case_table
+from pclib.test.test_srcs     import TestSrcCL
+from ocn_pclib.test.net_sinks import TestNetSinkCL
+from ocn_pclib.ifcs.packets   import mk_mesh_pkt
+from ocn_pclib.ifcs.positions import mk_mesh_pos
+from meshnet.MeshNetworkCL    import MeshNetworkCL
+from router.InputUnitCL       import InputUnitCL
+
+#-------------------------------------------------------------------------
+# TestHarness
+#-------------------------------------------------------------------------
+
+class TestHarness( Component ):
+
+  def construct( s, PktType, mesh_wid, mesh_ht, 
+                 src_msgs, sink_msgs, 
+                 src_initial, src_interval, 
+                 sink_initial, sink_interval ):
+
+    s.nrouters = mesh_wid * mesh_ht
+
+    MeshPos = mk_mesh_pos( mesh_wid, mesh_ht )
+    s.dut = MeshNetworkCL( PktType, MeshPos, mesh_wid, mesh_ht, 0 )
+
+    s.srcs  = [ TestSrcCL( src_msgs[i],  src_initial,  src_interval  )
+                for i in range ( s.nrouters ) ]
+    s.sinks = [ TestNetSinkCL( sink_msgs[i], sink_initial, sink_interval) 
+                for i in range ( s.nrouters ) ]
+
+    # Connections
+    for i in range ( s.nrouters ):
+      s.connect( s.srcs[i].send, s.dut.recv[i]   )
+      s.connect( s.dut.send[i],  s.sinks[i].recv )
+
+  def done( s ):
+    srcs_done = 1
+    sinks_done = 1
+    for i in range( s.nrouters ):
+      if s.srcs[i].done() == 0:
+        srcs_done = 0
+    for i in range( s.nrouters ):
+      if s.sinks[i].done() == 0:
+        sinks_done = 0
+    return srcs_done and sinks_done
+
+  def line_trace( s ):
+    return s.dut.line_trace()
+
+#-------------------------------------------------------------------------
+# run_rtl_sim
+#-------------------------------------------------------------------------
+
+def run_sim( test_harness, max_cycles=100 ):
+
+  # Create a simulator
+
+  test_harness.apply( SimpleSim )
+  test_harness.sim_reset()
+
+  # Run simulation
+
+  ncycles = 0
+  print ""
+  print "{:2}:{}".format( ncycles, test_harness.line_trace() )
+  while not test_harness.done() and ncycles < max_cycles:
+    test_harness.tick()
+    ncycles += 1
+    print "{:2}:{}".format( ncycles, test_harness.line_trace() )
+
+  # Check timeout
+
+  assert ncycles < max_cycles
+
+  test_harness.tick()
+  test_harness.tick()
+  test_harness.tick()
+
+#-------------------------------------------------------------------------
+# Helper functions
+#-------------------------------------------------------------------------
+
+def mk_src_sink_msgs( pkts, mesh_wid, mesh_ht ):
+  nrouters = mesh_wid * mesh_ht
+  src_msgs  = [ [] for _ in range( nrouters ) ]
+  sink_msgs = [ [] for _ in range( nrouters ) ]
+  
+  for pkt in pkts:
+    src_id  = pkt.src_y * mesh_wid + pkt.src_x
+    sink_id = pkt.dst_y * mesh_wid + pkt.dst_x
+    src_msgs[ src_id ].append( pkt )
+    sink_msgs[ sink_id ].append( pkt )
+
+  return src_msgs, sink_msgs
+
+def mk_pkt_list( PktType, lst ):
+  ret = []
+  for m in lst:
+    src_x, src_y, dst_x, dst_y, opq, payload = m[0], m[1], m[2], m[3], m[4], m[5]
+    ret.append( PktType( src_x, src_y, dst_x, dst_y, opq, payload ) )
+  return ret
+
+#-------------------------------------------------------------------------
+# Test cases
+#-------------------------------------------------------------------------
+
+def simple_msg( PktType, mesh_wid=2, mesh_ht=2 ):
+  return mk_pkt_list( PktType, [
+  #   src_x src_y dst_x dst_y opq   payload
+    ( 0,    0,    0,    1,    0x00, 0x0010  ),
+    ( 1,    0,    1,    1,    0x01, 0x0020  ),
+  ])
+
+#-------------------------------------------------------------------------
+# test case table
+#-------------------------------------------------------------------------
+
+test_case_table = mk_test_case_table([
+  (            "msg_func    wid  ht  src_init src_intv sink_init sink_intv"),
+  ["simle_msg", simple_msg, 2,   2,  0,       0,       0,        0         ],
+])
+
+#-------------------------------------------------------------------------
+# run test
+#-------------------------------------------------------------------------
+
+@pytest.mark.parametrize( **test_case_table )
+def test_ring_simple( test_params ):
+  PktType = mk_mesh_pkt( 
+    mesh_wid=test_params.wid,
+    mesh_ht =test_params.ht,
+    nvcs=1,
+  )
+  pkt_list = test_params.msg_func( PktType, test_params.wid, test_params.ht )
+  src_msgs, sink_msgs = mk_src_sink_msgs( pkt_list, test_params.wid, test_params.ht )
+  th = TestHarness( 
+    PktType, 
+    test_params.wid,
+    test_params.ht,
+    src_msgs,
+    sink_msgs, 
+    test_params.src_init,
+    test_params.src_intv,
+    test_params.sink_init,
+    test_params.sink_intv,
+  )
+  run_sim( th )
