bfbf7bee719eb2dd4033f631224cf11cbd5d2952
ct535 <ct535@cornell.edu>
2019-06-12 00:42:19 -0400

    Implemented Characterization Pass for AET

--- meshnet/test/characterization_test.py
+++ meshnet/test/characterization_test.py
@@ -6,27 +6,16 @@
 # Author : Cheng Tan
 #   Date : June 10, 2019
 
-import hypothesis
-from hypothesis import strategies as st
-
 from pymtl3                        import *
-from pymtl3.stdlib.test.test_srcs  import TestSrcRTL
-from ocn_pclib.test.net_sinks      import TestNetSinkRTL
 from ocn_pclib.ifcs.positions      import mk_mesh_pos
-from ocn_pclib.ifcs.Packet         import Packet, mk_pkt 
+from ocn_pclib.ifcs.packets         import mk_mesh_pkt
 from pymtl3.stdlib.test            import TestVectorSimulator
 from meshnet.MeshRouterRTL        import MeshRouterRTL
-from meshnet.DORXMeshRouteUnitRTL import DORXMeshRouteUnitRTL
-#from meshnet.DORYMeshRouteUnitRTL import DORYMeshRouteUnitRTL
 from meshnet.DORYMeshRouteUnitRTL_wo_index import DORYMeshRouteUnitRTL
-from router.ULVCUnitRTL           import ULVCUnitRTL
-from router.InputUnitRTL          import InputUnitRTL
 from router.SwitchUnitRTL_wo_index          import SwitchUnitRTL
-
-from test_helpers import dor_routing
-
 from pymtl3.passes.sverilog import ImportPass, TranslationPass
 from pymtl3.passes.sverilog.util.test_utility import closed_loop_component_input_test
+from pymtl3.passes.CharacterizationPass import CharacterizationPass
 
 #-------------------------------------------------------------------------
 # characterization
@@ -36,8 +25,8 @@ def test_characterization():
 
   def tv_in( model, test_vector ):
 
-    MeshPos = mk_mesh_pos( 9, 9 )
-    pkt = mk_pkt( 1, 1, 2, 2, 1, 0 )
+    MeshPos = mk_mesh_pos( 4, 4 )
+    pkt = mk_mesh_pkt( 1, 1, 2, 2, 1, 0 )
 
     model.pos = MeshPos( 1, 1 )
     model.recv[4].msg = pkt
@@ -52,16 +41,18 @@ def test_characterization():
 
   test_vector = []
 
-  mesh_wid = mesh_ht = 9
+  mesh_wid = mesh_ht = 4
   MeshPos = mk_mesh_pos( mesh_wid, mesh_ht )
-  model = MeshRouterRTL( Packet, MeshPos, InputUnitType = InputUnitRTL, 
-          RouteUnitType = DORYMeshRouteUnitRTL, SwitchUnitType = SwitchUnitRTL )
+  PacketType = mk_mesh_pkt( 4, 4, 1, 8, 32 )
+  model = MeshRouterRTL( PacketType, MeshPos,
+                         RouteUnitType = DORYMeshRouteUnitRTL, 
+                         SwitchUnitType = SwitchUnitRTL )
 #  model.elaborate()
 #  model.sverilog_translate = True
 #  model.apply( SimpleSim )
 #  model.apply( TranslationPass() )
+  model.apply( CharacterizationPass() )
 
-  closed_loop_component_input_test( model, test_vector, tv_in )
-
+#  closed_loop_component_input_test( model, test_vector, tv_in )
 #  sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
 
