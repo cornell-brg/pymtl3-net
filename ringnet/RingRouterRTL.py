041aeead06e21a58d9c73c116e2565cafe5714a7
yo96 <yo96@cornell.edu>
2019-06-22 23:43:20 -0700

    use credit based interface

--- ringnet/RingRouterRTL.py
+++ ringnet/RingRouterRTL.py
@@ -7,6 +7,8 @@
 #   Date : April 6, 2019
 
 from pymtl3 import *
+
+from ocn_pclib.ifcs.CreditIfc import RecvIfcCreditRTL, SendIfcCreditRTL
 from router.Router import Router
 from router.SwitchUnitRTL import SwitchUnitRTL
 from router.InputUnitCreditRTL import InputUnitCreditRTL
@@ -25,13 +27,59 @@ class RingRouterRTL( Router ):
     OutputUnitType=OutputUnitCreditRTL
   ):
 
-    super( RingRouterRTL, s ).construct(
-      PacketType     = PacketType,
-      PositionType   = PositionType,
-      num_inports    = 3,
-      num_outports   = 3,
-      InputUnitType  = InputUnitType,
-      RouteUnitType  = RouteUnitType,
-      SwitchUnitType = SwitchUnitType,
-      OutputUnitType = OutputUnitRTL
+    s.num_inports  = num_inports
+    s.num_outports = num_outports
+
+    # Interface
+
+    s.pos  = InPort( PositionType )
+    s.recv = [ RecvIfcCreditRTL( PacketType ) for _ in range( s.num_inports  ) ]
+    s.send = [ SendIfcCreditRTL( PacketType ) for _ in range( s.num_outports ) ]
+
+    # Components
+
+    s.input_units  = [ InputUnitType( PacketType )
+                      for _ in range( s.num_inports ) ]
+
+    s.route_units  = [ RouteUnitType( PacketType, PositionType, s.num_outports )
+                      for i in range( s.num_inports ) ]
+
+    s.switch_units = [ SwitchUnitType( PacketType, s.num_inports )
+                      for _ in range( s.num_outports ) ]
+
+    s.output_units = [ OutputUnitType( PacketType )
+                      for _ in range( s.num_outports ) ]
+
+    # Connection
+
+    for i in range( s.num_inports ):
+      s.connect( s.recv[i],             s.input_units[i].recv )
+      s.connect( s.input_units[i].give, s.route_units[i].get  )
+      s.connect( s.pos,                 s.route_units[i].pos  )
+
+    for i in range( s.num_inports ):
+      for j in range( s.num_outports ):
+        s.connect( s.route_units[i].give[j], s.switch_units[j].get[i] )
+
+    for j in range( s.num_outports ):
+#      s.connect( s.switch_units[j].send, s.output_units[j].recv )
+      s.connect( s.switch_units[j].give, s.output_units[j].get )
+      s.connect( s.output_units[j].send, s.send[j]              )
+
+  # Line trace
+
+  def line_trace( s ):
+
+    in_trace  = [ "" for _ in range( s.num_inports  ) ]
+    out_trace = [ "" for _ in range( s.num_outports ) ]
+
+    for i in range( s.num_inports ):
+      in_trace[i]  = "{}".format( s.recv[i].line_trace() )
+    for i in range( s.num_outports ):
+      out_trace[i] = "{}".format( s.send[i].line_trace() )
+
+    return "{}({}){}".format(
+      "|".join( in_trace ),
+      s.pos,
+      "|".join( out_trace )
     )
