9a3eb81d189ed87f749e51cb299d87dcc16df8dd
yo96 <yo96@cornell.edu>
2019-07-12 15:45:53 -0400

    added openloop unit test.

--- examples/pyh2_test/QueueVRTL_test.py
+++ examples/pyh2_test/QueueVRTL_test.py
@@ -11,13 +11,15 @@ from __future__ import absolute_import, division, print_function
 
 from pymtl3 import *
 from pymtl3.dsl import Placeholder
+from pymtl3.passes import GenDAGPass, OpenLoopCLPass
 from pymtl3.passes.sverilog import ImportPass
 from pymtl3.stdlib.cl.queues import NormalQueueCL
+from pymtl3.stdlib.rtl.queues import NormalQueueRTL
 from pymtl3.stdlib.ifcs import DeqIfcRTL, EnqIfcRTL
-from pymtl3.stdlib.test.test_sinks import TestSinkRTL
+from pymtl3.stdlib.test.test_sinks import TestSinkCL, TestSinkRTL
 from pymtl3.stdlib.test.test_srcs import TestSrcCL
 from pymtl3.stdlib.test.pyh2.stateful import run_pyh2
-
+from pymtl3.stdlib.test.pyh2.RTL2CLWrapper import RTL2CLWrapper
 from .QueueVRTL import Queue, QueueVRTL
 
 
@@ -57,6 +59,29 @@ class TestHarness( Component ):
     return "{} ({}) {}".format(
       s.src.line_trace(), s.dut.line_trace(), s.sink.line_trace() )
 
+class TestHarnessWrapped( Component ):
+
+  def construct( s, MsgType, QType, src_msgs, sink_msgs ):
+
+    s.src  = TestSrcCL( MsgType, src_msgs )
+    s.dut  = RTL2CLWrapper( QType(), { 'enq': None, 'deq': None } )
+    s.sink = TestSinkCL( MsgType, sink_msgs )
+
+    s.connect( s.src.send, s.dut.enq   )
+
+    @s.update
+    def up_deq_recv():
+      if s.dut.deq.rdy() and s.sink.recv.rdy():
+        s.sink.recv( s.dut.deq() )
+
+  def done( s ):
+    return s.src.done() and s.sink.done()
+
+  def line_trace( s ):
+    return "{} ({}) {}".format(
+      s.src.line_trace(), s.dut.line_trace(), s.sink.line_trace() )
+
+
 #-------------------------------------------------------------------------
 # run_sim
 #-------------------------------------------------------------------------
@@ -71,6 +96,7 @@ def run_sim( th, max_cycles=100 ):
   th.sim_reset()
 
   print("")
+  print( "SCHEDULE:", th._sched.schedule )
   ncycles = 0
   print("{:2}:{}".format( ncycles, th.line_trace() ))
   while not th.done() and ncycles < max_cycles:
@@ -89,17 +115,40 @@ def run_sim( th, max_cycles=100 ):
 # Test cases
 #-------------------------------------------------------------------------
 
-test_msgs = [ Bits32( 4 ), Bits32( 1 ), Bits32( 2 ), Bits32( 3 ) ]
-
-def test_normal1_simple():
+def test_simple():
+  test_msgs = [ b32(4), b32(1), b32(2), b32(3), b32(4), b32(5) ]
   th = TestHarness( Bits32, QueueVRTL, test_msgs, test_msgs )
   run_sim( th )
+
+def test_simple_wrapped():
+  test_msgs = [ b32(4), b32(1), b32(2), b32(3), b32(4), b32(5) ]
+  th = TestHarnessWrapped( Bits32, QueueVRTL, test_msgs, test_msgs )
+  run_sim( th )
+
+def test_openloop():
+  print()
+  dut = RTL2CLWrapper( QueueVRTL(), { 'enq': None, 'deq': None } )
+  dut.elaborate()
+  dut = ImportPass()( dut )
+  dut.elaborate()
+  dut.apply( GenDAGPass() )
+  dut.apply( OpenLoopCLPass() )
+  dut.lock_in_simulation()
+  dut.tick()
+  dut.tick()
+  dut.sim_reset()
+  dut.tick()
+  assert dut.enq.rdy()
+  dut.enq( 0x1111 )
+  assert dut.enq.rdy()
+  dut.enq( 0x2222 )
 
 #-------------------------------------------------------------------------
 # PyH2 test
 #-------------------------------------------------------------------------
 
 def test_pyh2():
-  sv_q = QueueVRTL()
+  # sv_q = QueueVRTL()
+  sv_q = NormalQueueRTL( Bits32, num_entries=2 )
   cl_q = NormalQueueCL( num_entries=2 )
   run_pyh2( sv_q, cl_q )
