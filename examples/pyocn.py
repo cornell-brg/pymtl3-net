e241a421827dc97ca68eca8623255eb6a2a61bf0
tancheng <tancheng1990@gmail.com>
2019-09-01 23:03:43 -0400

    initially implemented generation of Mesh network with dumped VCD file

--- examples/pyocn.py
+++ examples/pyocn.py
@@ -54,8 +54,9 @@ seed(0xdeadbeef)
 # generate
 #--------------------------------------------------------------------------
 
-def generate():
-  pass
+def generate( model, topology, nodes, rows, channel_lat, packets ):
+  perform( 'generate', model, topology, nodes, rows, channel_lat, 0,
+           None, packets )
 
 #--------------------------------------------------------------------------
 # characterize
@@ -68,14 +69,18 @@ def characterize():
 # simulate
 #--------------------------------------------------------------------------
 
+def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
+  return perform( 'simulate', model, topology, nodes, rows, channel_lat, injection,
+                  pattern, None )
+
 # Global constants for simulation
 
 NUM_WARMUP_CYCLES   = 10
 NUM_SAMPLE_CYCLES   = 50 + NUM_WARMUP_CYCLES
 INVALID_TIMESTAMP   = 0
 
-#def simulate( opts, injection_rate, pattern, drain_limit, dump_vcd, trace, verbose ):
-def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
+def perform( action, model, topology, nodes, rows, channel_lat, injection,
+             pattern, packets ):
 
   routers         = nodes
   channel_latency = channel_lat
@@ -84,7 +89,7 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
 
   # Simulation Variables
 
-  average_latency       = 0
+  average_latency       = 0.0
   packets_generated     = 0
   packets_received      = 0
   all_packets_received  = 0
@@ -112,8 +117,8 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
     if topology  == "Ring":
       NetModel   = RingNetworkRTL
       RingPos    = mk_ring_pos( routers )
-      PacketType = mk_ring_pkt_timestamp( routers, nvcs = 2,
-                   max_time = NUM_SAMPLE_CYCLES )
+      PacketType = mk_ring_pkt_timestamp( routers, payload_nbits = 32,
+                   nvcs = 2, max_time = NUM_SAMPLE_CYCLES )
       net        = NetModel( PacketType, RingPos, routers, channel_lat )
 #      net.set_param( "top.routers*.route_units*.construct", num_routers=routers)
 
@@ -132,7 +137,8 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
       net_width   = routers // rows
       net_height  = rows
       MeshPos     = mk_mesh_pos( net_width, net_height )
-      PacketType  = mk_mesh_pkt_timestamp( net_width, net_height, nvcs = 2,
+      PacketType  = mk_mesh_pkt_timestamp( net_width, net_height,
+                    payload_nbits = 32, nvcs = 2,
                     max_time = NUM_SAMPLE_CYCLES )
       net         = NetModel( PacketType, MeshPos, net_width, net_height, 0 )
       # model.set_param('top.routers*.route_units*.construct', ncols=net_width )
@@ -149,7 +155,7 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
       outports    = term_each + 4
       MeshPos     = mk_mesh_pos( net_width, net_height )
       PacketType  = mk_cmesh_pkt_timestamp( net_width, net_height,
-                    inports, outports, payload_nbits = 2,
+                    inports, outports, payload_nbits = 32,
                     max_time = NUM_SAMPLE_CYCLES )
       net         = NetModel( PacketType, MeshPos, net_width, net_height,
                     term_each, 0 )
@@ -162,26 +168,21 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
       num_routers = n_fly * ( k_ary ** ( n_fly - 1 ) )
       MeshPos     = mk_bfly_pos( k_ary, n_fly )
       PacketType  = mk_bfly_pkt_timestamp( k_ary, n_fly,
-                    payload_nbits = 1, max_time = NUM_SAMPLE_CYCLES )
+                    payload_nbits = 32, max_time = NUM_SAMPLE_CYCLES )
       net         = NetModel( PacketType, MeshPos, k_ary, n_fly, 0 )
       net.set_param( "top.routers*.construct", k_ary=k_ary )
       net.set_param( "top.routers*.route_units*.construct", n_fly=n_fly )
 
   net.elaborate()
-  net.sverilog_translate = True
-  net.dump_vcd = True
-  net.vcd_file_name = "dumpVCD"
-  net.apply( TranslationPass() )
+  if action == "generate":
+    net.sverilog_translate = True
+    net.dump_vcd = True
+    net.vcd_file_name = "dumpVCD"
+    net.apply( TranslationPass() )
   sim = net.apply( SimulationPass )
-#  sim = net.apply( DynamicSim )
-#  test_harness.dut.sverilog_translate = True
-#  test_harness.dut.sverilog_import = True
-#  test_harness.apply( TranslationPass() )
-#  test_harness = ImportPass()( test_harness )
-#  test_harness.apply( SimpleSim )
 
   # Source Queues - Modeled as Bypass Queues
-  src = [ deque() for x in range( nodes ) ]
+  src_queue = [ deque() for x in range( nodes ) ]
 
   # Run the simulation
 
@@ -202,50 +203,60 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
 
       # Generate packet
 
-      if ( randint( 1, 100 ) <= injection_rate ):
+      if ( (action == "simulate" and randint( 1, 100 ) <= injection_rate) or
+           (action == "generate" and packets[0]['src'] == i) ):
 
-        # traffic pattern based dest selection
-        if   pattern == "urandom":
-          dest = randint( 0, nodes-1 )
-        elif pattern == "partition":
-          dest = ( randint( 0, nodes-1 ) ) & (nodes // 2-1) |\
-                 ( i & (nodes // 2) )
-        elif pattern == "opposite":
-          dest = ( i + 2 ) % nodes
-        elif pattern == "neighbor":
-          dest = ( i + 1 ) % nodes
-        elif pattern == "complement":
-          dest = i ^ (nodes-1)
+        if (action == "simulate"):
+          # traffic pattern based dest selection
+          if   pattern == "urandom":
+            dest = randint( 0, nodes-1 )
+          elif pattern == "partition":
+            dest = ( randint( 0, nodes-1 ) ) & (nodes // 2-1) |\
+                   ( i & (nodes // 2) )
+          elif pattern == "opposite":
+            dest = ( i + 2 ) % nodes
+          elif pattern == "neighbor":
+            dest = ( i + 1 ) % nodes
+          elif pattern == "complement":
+            dest = i ^ (nodes-1)
+          src = i
+          data = Bits32(6)
+  
+        if (action == "generate"):
+          src  = packets[0]['src']
+          dest = packets[0]['dest']
+          data = Bits32(packets[0]['data'])
+          packets.popleft()
 
         # inject packet past the warmup period
-
+  
         timestamp = INVALID_TIMESTAMP
         if ( NUM_WARMUP_CYCLES < ncycles < NUM_SAMPLE_CYCLES ):
           timestamp = ncycles
-
+  
         if topology == "Ring":
-          pkt = PacketType( i, dest, 0, 0, 98+i+ncycles, timestamp )
-
+          pkt = PacketType( src, dest, 0, 0, data, timestamp )
+  
         elif topology == "Mesh":
           XYType = mk_bits( clog2( net_width ) )
           timeType = mk_bits(clog2(NUM_SAMPLE_CYCLES+1))
-          pkt = PacketType( XYType(i%net_width), XYType(i//net_width), 
+          pkt = PacketType( XYType(src%net_width), XYType(src//net_width), 
                   XYType(dest % net_width), XYType(dest//net_width), 
-                  Bits8(0), Bits32(6), timeType(timestamp) )
-
-
+                  Bits8(0), data, timeType(timestamp) )
+  
+  
         elif topology == "Torus":
-          pkt = PacketType( i % net_width, i // net_width, dest % net_width,
-                  dest // net_width, 0, 0, 6, timestamp )
-
+          pkt = PacketType( src % net_width, src // net_width, dest % net_width,
+                  dest // net_width, 0, 0, data, timestamp )
+  
         elif topology == "CMesh":
-          pkt = PacketType( (i // term_each) %  net_width,
-                            (i // term_each) // net_width,
+          pkt = PacketType( (src // term_each) %  net_width,
+                            (src // term_each) // net_width,
                             (dest // term_each) %  net_width,
                             (dest // term_each) // net_width,
                             dest % term_each,
-                            0, 6, timestamp )
-
+                            0, data, timestamp )
+  
         elif topology == "Bfly":
           r_rows = k_ary ** ( n_fly - 1 )
           if r_rows == 1 or k_ary == 1:
@@ -264,17 +275,17 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
                 bf_dst = bf_dst * 2
               else:
                 bf_dst = bf_dst * k_ary
-          pkt = PacketType( i, bf_dst, 0, 6, timestamp )
-
-        src[i].append( pkt )
+          pkt = PacketType( src, bf_dst, 0, data, timestamp )
+  
+        src_queue[src].append( pkt )
         if ( ncycles < NUM_SAMPLE_CYCLES ):
           packets_generated += 1
 
-      # Inject from source queue
+        # Inject from source queue
 
-      if ( len( src[i] ) > 0 ):
+      if ( len( src_queue[i] ) > 0 ):
         if net.recv[i].rdy:
-          net.recv[i].msg = src[i][0]
+          net.recv[i].msg = src_queue[i][0]
           net.recv[i].en  = Bits1(1)
         else:
           net.recv[i].en  = Bits1(0)
@@ -295,16 +306,21 @@ def simulate( model, topology, nodes, rows, channel_lat, injection, pattern ):
 
       # Check if finished - drain phase
 
-      if ( ncycles >= NUM_SAMPLE_CYCLES and
-           all_packets_received >= packets_generated ):
-        average_latency = int( total_latency ) / float( packets_received )
-        sim_done = True
-        break
+      if action == 'simulate':
+        if ( ncycles >= NUM_SAMPLE_CYCLES and
+             all_packets_received >= packets_generated ):
+          average_latency = int( total_latency ) / float( packets_received )
+          sim_done = True
+          break
+      elif action == 'generate':
+        if ( all_packets_received >= packets_generated ):
+          sim_done = True
+          break
 
       # Pop the source queue
 
-      if ( net.recv[i].rdy == Bits1(1) ) and ( len( src[i] ) > 0 ):
-        src[i].popleft()
+      if ( net.recv[i].rdy == Bits1(1) ) and ( len( src_queue[i] ) > 0 ):
+        src_queue[i].popleft()
 
     # print line trace if enables
 
@@ -336,17 +352,14 @@ def main():
   for action in config['action']:
 
     if action == 'generate':
-      # TODO: Generating Verilog
       print()
       print( "[GENERATE]" )
       print( "=======================================================================================" )
-      generate()
-
-    if action == 'characterize':
-      # TODO: Use the backend script to characterize the target network?
-      print()
-      print( "[CHARACTERIZE]" )
-      print( "=======================================================================================" )
+      packets = config['packets']
+      for model in config['model']:
+        for topology in config['topology']:
+          results = generate( model, topology, config['nodes'], config['rows'],
+                    config['channel_lat'], packets )
 
     if action == 'simulate':
 
