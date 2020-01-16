667ab660f968c141ce295bcce5296a6abb0d308e
yo96 <yo96@cornell.edu>
2020-01-16 16:14:50 -0500

    not dump verilator vcd by default

--- ocn_pclib/test/sim_utils.py
+++ ocn_pclib/test/sim_utils.py
@@ -5,23 +5,25 @@ from pymtl3.passes.backends.yosys import TranslationImportPass as YSTransImport
 from pymtl3.passes.backends.yosys import ImportConfigs as YSConfig
 
 # Assumes th.dut
-def run_sim( th, max_cycles=1000, translation='', trace=True ):
+def run_sim( th, max_cycles=1000, translation='', trace=True, verilator_vcd=False ):
 
   th.elaborate()
 
   if translation == 'sverilog':
     TransImport = SVTransImport
     th.dut.sverilog_translate_import = True
-    th.dut.config_sverilog_import = SVConfig(
-      vl_trace = True,
-    )
+    if verilator_vcd:
+      th.dut.config_sverilog_import = SVConfig(
+        vl_trace = True,
+      )
 
   elif translation == 'yosys':
     TransImport = YSTransImport
     th.dut.yosys_translate_import = True
-    th.dut.config_yosys_import = SVConfig(
-      vl_trace = True,
-    )
+    if verilator_vcd:
+      th.dut.config_yosys_import = YSConfig(
+        vl_trace = True,
+      )
 
   if translation:
     th = TransImport()( th )
