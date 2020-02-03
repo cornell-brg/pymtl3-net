d21531aca3c6b9af8a872f551e39e33f1d6fa060
yo96 <yo96@cornell.edu>
2020-02-03 14:29:43 -0500

    unit test for multi flit packet

---
+++ ocnlib/packets/MultiFlitPacket_test.py
@@ -0,0 +1,71 @@
+'''
+==========================================================================
+MultiFlitPacket_test.py
+==========================================================================
+Unit tests for MultiFlitPacket.
+
+Author : Yanghui Ou
+  Date : Jan 31, 2019
+'''
+from pymtl3 import *
+
+from .packet_formats import packet_format
+from .MultiFlitPacket import MultiFlitPacket
+from .OpenPitonFormat import OpenPitonFormat
+
+@packet_format( 32 )
+class SimpleFormat:
+  plen   : (0,  8 )
+  opaque : (8,  16)
+  id     : (16, 24)
+
+def test_add():
+  # An empty packet
+  pkt = MultiFlitPacket( SimpleFormat )
+  assert pkt.empty()
+
+  header      = b32(0) 
+  header[0:8] = b8(3)
+  pkt.add( header )
+  pkt.add( b32(0xdeadbeef) )
+  assert not pkt.empty()
+  pkt.add( b32(0xfaceb00c) )
+  assert not pkt.empty()
+  pkt.add( b32(0xfeedbabe) )
+  assert not pkt.empty()
+
+  assert pkt.full()
+  assert pkt.flits[1] == 0xdeadbeef
+  assert pkt.flits[2] == 0xfaceb00c
+  assert pkt.flits[3] == 0xfeedbabe
+
+def test_pop():
+  # A single-flit packet
+  header = b32(0)
+  pkt    = MultiFlitPacket( SimpleFormat, [ header ] )
+  assert not pkt.empty()
+  assert pkt.full()
+  assert pkt.pop() == b32(0)
+  assert pkt.empty()
+  assert not pkt.full()
+
+  # A multi-flit packet
+  header      = b32(0)
+  header[0:8] = b8(2)
+  flits       = [ header, b32(0xfaceb00c), b32(0x8badf00d) ]
+  pkt         = MultiFlitPacket( SimpleFormat, flits )
+
+  assert not pkt.empty()
+  assert pkt.full()
+
+  assert pkt.pop() == b32(2)
+  assert not pkt.full()
+  assert not pkt.empty()
+
+  assert pkt.pop() == b32(0xfaceb00c)
+  assert not pkt.full()
+  assert not pkt.empty()
+
+  assert pkt.pop() == b32(0x8badf00d)
+  assert not pkt.full()
+  assert pkt.empty()
