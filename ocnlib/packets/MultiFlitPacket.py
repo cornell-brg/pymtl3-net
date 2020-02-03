1221e1ccb2e441662939b1e70b83393eec9bf8ff
yo96 <yo96@cornell.edu>
2020-02-03 13:55:37 -0500

    small fix

--- ocnlib/packets/MultiFlitPacket.py
+++ ocnlib/packets/MultiFlitPacket.py
@@ -14,8 +14,8 @@ Author : Yanghui Ou
 #-------------------------------------------------------------------------
 
 def _get_payload_length( Format, header_flit, field_name='plen' ):
-  plen_slice = getattr( Format, plen_field_name )
-  return header[ plen_slice ].uint()
+  plen_slice = getattr( Format, field_name )
+  return header_flit[ plen_slice ].uint()
 
 #-------------------------------------------------------------------------
 # MultiFlitPacket
@@ -25,32 +25,35 @@ class MultiFlitPacket:
 
   def __init__( self, Format, flits=[], plen_field_name='plen' ):
 
-    self.PhitType = Format.PhitType
-    self.add_lock = False
-    self.pop_lock = False
-    self.nflits   = 0
-    self.flit_idx = 0
-    self.flits    = [ self.PhitType(f) for f in flits ]
+    self.Format     = Format
+    self.plen_fname = plen_field_name 
+    self.PhitType   = Format.PhitType
+    self.add_lock   = False
+    self.pop_lock   = False
+    self.nflits     = 0
+    self.flit_idx   = 0
+    self.flits      = [ self.PhitType(f) for f in flits ]
 
     # Check
     if self.flits:
       self._get_nflits()
+      assert nflits == len( self.flits ) - 1
 
   def add( self, flit ):
-    assert not pop_lock, "Packet locked by pop, cannot add any more!"
+    assert not self.pop_lock, "Packet locked by pop, cannot add any more!"
 
     # Adding header flit
     if self.empty():
-      self.flits.append( flits )
+      self.flits.append( flit )
       self._get_nflits()
     else:
       assert not self.full(), "Packet is already full" 
-      self.flits.append( flits )
+      self.flits.append( flit )
 
     self.add_lock = True
 
   def pop( self ):
-    assert not add_lock, "Packet locked by add, cannot pop any more!"
+    assert not self.add_lock, "Packet locked by add, cannot pop any more!"
     assert self.flit_idx < self.nflits, "Already reached the last flit of the packet!" 
     # Copy the current flit
     cur_flit = self.PhitType( self.flits[ self.flit_idx ] )
@@ -60,12 +63,11 @@ class MultiFlitPacket:
     return cur_flit
 
   def full( self ):
-    return s.nflits > 0 and s.nflits == len( self.flits )
+    return self.nflits > 0 and self.nflits == len( self.flits )
 
   def empty( self ):
-    return len( self.flits == 0 ) 
+    return len( self.flits ) == 0
 
   def _get_nflits( self ):
-    nflits = _get_payload_length( Format, self.flits[0], plen_field_name )
-    assert nflits == len( self.flits ) - 1
+    nflits = _get_payload_length( self.Format, self.flits[0], self.plen_fname ) + 1
     self.nflits = nflits
