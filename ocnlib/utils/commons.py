e88feafe7900fb671b08718ead081c8b151d32fa
yo96 <yo96@cornell.edu>
2020-02-11 14:17:20 -0500

    add to_bits and to_bitstruct

--- ocnlib/utils/commons.py
+++ ocnlib/utils/commons.py
@@ -7,15 +7,18 @@ Common utility functions.
 Author : Yanghui Ou
   Date : Feb 9, 2020
 '''
+from functools import reduce
 from pymtl3 import *
 from pymtl3.datatypes.bitstructs import(
   is_bitstruct_class, 
+  is_bitstruct_inst, 
   _FIELDS as bitstruct_fields,
 )
 
 #-------------------------------------------------------------------------
 # get_nbits
 #-------------------------------------------------------------------------
+# TODO: support instance too
 
 def get_nbits( cls ):
   if issubclass( cls, Bits ):
@@ -29,11 +32,11 @@ def get_nbits( cls ):
       total_nbits +=  get_nbits( ftype )
     return total_nbits
 
-
 #-------------------------------------------------------------------------
 # get_plen_type
 #-------------------------------------------------------------------------
 # Returns the type of feild payload length from the header format.
+# FIXME: hacky.
 
 def get_plen_type( cls, plen_field_name='plen' ):
   assert is_bitstruct_class( cls )
@@ -44,4 +47,90 @@ def get_plen_type( cls, plen_field_name='plen' ):
 
   return fields_dict[ plen_field_name ]
 
+#------------------------------------------------------------------------- 
+# bitstruct_to_slice_h
+#------------------------------------------------------------------------- 
+# Recursive helper function
+
+def _bitstruct_to_slices_h( cls, slices ): 
+  if issubclass( cls, Bits ):
+    start = 0 if not slices else slices[-1].stop
+    stop  = start + cls.nbits
+    slices.append( slice( start, stop ) )
+
+  else:
+    assert is_bitstruct_class( cls )
+    fields_dict = getattr( cls, bitstruct_fields )
+    fields_lst  = list( fields_dict.items() )
+    # Use reverse instead of [::-1] because it is faster
+    fields_lst.reverse()
+    for _, ftype in fields_lst:
+      _bitstruct_to_slices_h( ftype, slices )
+
+#------------------------------------------------------------------------- 
+# bitstruct_to_slice
+#------------------------------------------------------------------------- 
+# Converts a bitstruct to a list of slices
+
+def bitstruct_to_slices( cls ):
+  slices = []
+  _bitstruct_to_slices_h( cls, slices )
+  return slices
+
+#-------------------------------------------------------------------------
+# to_bits
+#-------------------------------------------------------------------------
+# Converts a bitstruct object to bits
+
+def to_bits( obj ):
+  if isinstance( obj, Bits ):
+    # FIXME: call clone here once pymtl3 is updated
+    return obj.__class__( obj )
+
+  else:
+    assert is_bitstruct_inst( obj )
+    fields_dict = getattr( obj, bitstruct_fields )
+
+    acc = []
+    for fname, ftype in fields_dict.items():
+      bits = to_bits( getattr( obj, fname ) )
+      acc.append( bits )
+
+    return reduce( lambda a, b: concat( a, b ), acc )
+
+#-------------------------------------------------------------------------
+# _to_bitstruct_h
+#-------------------------------------------------------------------------
+# Recursive helper function for to_bitstruct.
+# NOTE: I am being very conservative here to copy every field.
+
+def _to_bitstruct_h( field_type, bits_obj, slices_stack ):
+  if issubclass( field_type, Bits ):
+    cur_slice = slices_stack.pop()
+    return field_type( bits_obj[ cur_slice ] )
+
+  else:
+    assert is_bitstruct_class( field_type )
+    ret = field_type()
+    fields_dict = getattr( field_type, bitstruct_fields )
+    for fname, ftype in fields_dict.items():
+      field_value = _to_bitstruct_h( ftype, bits_obj, slices_stack )
+      setattr( ret, fname, field_value )
+    return ret
+
+#-------------------------------------------------------------------------
+# to_bitstruct
+#-------------------------------------------------------------------------
+# Converts a bit or bitstruct object to bitstruct
+# TODO: more informative error message.
+
+def to_bitstruct( obj, BitstructType ):
+  bits_obj = to_bits( obj )
+  assert isinstance( bits_obj, Bits )
+
+  slices_stack = bitstruct_to_slices( BitstructType )
+  assert bits_obj.nbits == slices_stack[-1].stop, "Bit width mismatch!"
+  ret = _to_bitstruct_h( BitstructType, bits_obj, slices_stack )
+  assert not slices_stack # Make sure every field is assigned
+  return ret
 
--- ocnlib/utils/commons_test.py
+++ ocnlib/utils/commons_test.py
@@ -8,7 +8,7 @@ Author : Yanghui Ou
   Date : Feb 10, 2020
 '''
 from pymtl3 import *
-from .commons import get_nbits
+from .commons import get_nbits, to_bits, to_bitstruct
 
 #-------------------------------------------------------------------------
 # Bitstruct for unit tests
@@ -18,6 +18,11 @@ from .commons import get_nbits
 class Point:
   x : Bits4
   y : Bits8
+
+@bitstruct
+class PointAlt:
+  x : Bits8
+  y : Bits4
 
 @bitstruct
 class Nested:
@@ -31,4 +36,32 @@ class Nested:
 def test_get_nbits():
   assert get_nbits( Point  ) == 12
   assert get_nbits( Nested ) == 20
+
+#-------------------------------------------------------------------------
+# test_to_bits
+#-------------------------------------------------------------------------
+
+def test_to_bits():
+  pt = Point( 0xe, 0xdc )
+  nested = Nested( pt, z=0xba )
+  assert to_bits( pt ) == 0xedc
+  assert to_bits( nested ) == 0xedcba
+
+def test_to_bitstruct():
+  bits = b12( 0xcba )
+  pt1 = to_bitstruct( bits, Point    )
+  assert pt1.x == 0xc
+  assert pt1.y == 0xba
+
+  pt2 = to_bitstruct( bits, PointAlt )
+  assert pt2.x == 0xcb
+  assert pt2.y == 0xa
+
+  pt3 = to_bitstruct( pt1, PointAlt )
+  assert pt3.x == 0xcb
+  assert pt3.y == 0xa
+
+  pt4 = to_bitstruct( pt2, Point    )
+  assert pt4.x == 0xc
+  assert pt4.y == 0xba
 
