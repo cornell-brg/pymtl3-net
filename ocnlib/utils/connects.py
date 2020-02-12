6dd92368ffa91eadcbb84ff1cfcce36fa8296d68
yo96 <yo96@cornell.edu>
2020-02-12 17:05:24 -0500

    optimize connect_bitstruct a bit: only traverse once

--- ocnlib/utils/connects.py
+++ ocnlib/utils/connects.py
@@ -109,22 +109,46 @@ def bitstruct_to_slices( cls ):
 # TODO: it might be more efficent to just traverse the tree once.
 # TODO: use get_type() API once it is added in pymtl3.
 
-def _connect_bitstruct_h( field, bits_signal, slices_stack ):
+# def _connect_bitstruct_h( field, bits_signal, slices_stack ):
+#   field_type = field._dsl.Type
+# 
+#   # Base case: current field is a BitsN
+#   if not is_bitstruct_class( field_type ):
+#     assert issubclass( field_type, Bits )
+#     connect( field, bits_signal[ slices_stack.pop() ] )
+#     return
+# 
+#   # Recursive case: current field is a bitstruct
+#   else:
+#     fields_dict = getattr( field_type, bitstruct_fields )
+#     for fname, ftype in fields_dict.items():
+#       cur_field = getattr( field, fname )
+#       _connect_bitstruct_h( cur_field, bits_signal, slices_stack )
+#     return
+
+#------------------------------------------------------------------------- 
+# _connect_bitstruct_h
+#------------------------------------------------------------------------- 
+
+def _connect_bitstruct_h( field, bits_signal, slices ): 
   field_type = field._dsl.Type
 
-  # Base case: current field is a BitsN
-  if not is_bitstruct_class( field_type ):
-    assert issubclass( field_type, Bits )
-    connect( field, bits_signal[ slices_stack.pop() ] )
-    return
+  if issubclass( field_type, Bits ):
+    start     = 0 if not slices else slices[-1].stop
+    stop      = start + field_type.nbits
+    cur_slice = slice( start, stop )
+    slices.append( cur_slice )
+    connect( field, bits_signal[ cur_slice ] )
 
-  # Recursive case: current field is a bitstruct
   else:
+    assert is_bitstruct_class( field_type )
     fields_dict = getattr( field_type, bitstruct_fields )
-    for fname, ftype in fields_dict.items():
-      cur_field = getattr( field, fname )
-      _connect_bitstruct_h( cur_field, bits_signal, slices_stack )
-    return
+    fields_lst  = list( fields_dict.items() )
+    # Use reverse instead of [::-1] because it is faster
+    fields_lst.reverse()
+    for fname, _ in fields_lst:
+      subfield = getattr( field, fname )
+      _connect_bitstruct_h( subfield, bits_signal, slices )
 
 #------------------------------------------------------------------------- 
 # connect_bitstruct
@@ -143,9 +167,7 @@ def connect_bitstruct( signal1, signal2 ):
   else:
     assert False, "Can only connect a bitstruct wire to bits wire"
 
-  slices = bitstruct_to_slices( bitstruct_signal._dsl.Type )
-
   # Connect field to corresponding slice
-  _connect_bitstruct_h( bitstruct_signal, bits_signal, slices )
+  _connect_bitstruct_h( bitstruct_signal, bits_signal, [] )
 
 
