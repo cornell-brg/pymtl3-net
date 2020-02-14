046f6cea1626649ab0e485dbbe0e98a48d40c8a4
yo96 <yo96@cornell.edu>
2020-02-13 20:16:08 -0500

    mulit-flit FL

--- meshnet/MeshRouterFL.py
+++ meshnet/MeshRouterFL.py
@@ -8,9 +8,15 @@ Author : Yanghui Ou
   Date : July 3, 2019
 """
 from pymtl3 import *
+from ocnlib.utils import to_bitstruct
 
 from .directions import *
 
+
+#-------------------------------------------------------------------------
+# MeshRouterFL
+#-------------------------------------------------------------------------
+# Mesh router that supports single flit packet.
 
 class MeshRouterFL:
 
@@ -78,5 +84,89 @@ class MeshRouterFL:
             SOUTH
           )
           dst_pkts[ dst ].append( pkt )
+
+    return dst_pkts
+
+#-------------------------------------------------------------------------
+# MeshRouterFL
+#-------------------------------------------------------------------------
+# Mesh router that supports single flit packet.
+
+class MeshRouterMFlitFL:
+
+  def __init__( s, Header, pos_x, pos_y, first_dimension='x' ):
+    s.Header          = Header
+    s.pos_x           = pos_x
+    s.pos_y           = pos_y
+    s.first_dimension = first_dimension
+
+  def arrange_src_pkts( s, lst ):
+    src_pkts = [ [] for _ in range(5) ]
+    if s.first_dimension == 'y':
+      for pkt in lst:
+        pkt = to_bitstruct( pkt, s.Header )
+        if pkt.src_x == s.pos_x and pkt.src_y == s.pos_y:
+          src_pkts[ SELF ].append( pkt )
+        elif pkt.src_x == s.pos_x:
+          if pkt.src_y < s.pos_y:
+            src_pkts[ SOUTH ].append( pkt )
+          else:
+            src_pkts[ NORTH ].append( pkt )
+        elif pkt.src_x < s.pos_x:
+          src_pkts[ WEST ].append( pkt )
+        else:
+          src_pkts[ EAST ].append( pkt )
+
+    elif s.first_dimension == 'x':
+      for pkt in lst:
+        pkt = to_bitstruct( pkt, s.Header )
+        if pkt.src_x == s.pos_x and pkt.src_y == s.pos_y:
+          src_pkts[ SELF ].append( pkt )
+        elif pkt.src_y == s.pos_y:
+          if pkt.src_x < s.pos_x:
+            src_pkts[ WEST ].append( pkt )
+          else:
+            src_pkts[ NORTH ].append( pkt )
+        elif pkt.src_y < s.pos_y:
+          src_pkts[ SOUTH ].append( pkt )
+        else:
+          src_pkts[ NORTH ].append( pkt )
+    else:
+      assert False
+
+    return src_pkts
+
+  def route( s, src_pkts ):
+    assert len( src_pkts ) == 5
+    dst_pkts = [ [] for _ in range(5) ]
+
+    if s.first_dimension == 'y':
+      for pkts in src_pkts:
+        for pkt in pkts:
+          pkt = to_bitstruct( pkt, s.Header )
+          dst = (
+            SELF  if pkt.dst_x == s.pos_x and pkt.dst_y == s.pos_y else
+            NORTH if pkt.dst_y > s.pos_y else
+            SOUTH if pkt.dst_y < s.pos_y else
+            EAST  if pkt.dst_x > s.pos_x else
+            WEST
+          )
+          dst_pkts[ dst ].append( pkt )
+
+    elif s._firstdimension == 'x':
+      for pkts in src_pkts:
+        for pkt in pkts:
+          pkt = to_bitstruct( pkt, s.Header )
+          dst = (
+            SELF  if pkt.dst_x == s.pos_x and pkt.dst_y == s.pos_y else
+            EAST  if pkt.dst_x > s.pos_x else
+            WEST  if pkt.dst_x < s.pos_x else
+            NORTH if pkt.dst_y > s.pos_y else
+            SOUTH
+          )
+          dst_pkts[ dst ].append( pkt )
+
+    else:
+      assert False
 
     return dst_pkts
