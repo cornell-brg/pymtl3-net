fb483a474ec5b66d66322ed0b2689bb32e46a827
yo96 <yo96@cornell.edu>
2020-02-17 16:06:27 -0500

    boundary unit to solve dangling cl port

---
+++ ocnlib/cl/BoundaryUnit.py
@@ -0,0 +1,19 @@
+'''
+==========================================================================
+BoundaryUnit.py
+==========================================================================
+Error check unit that solves the dangling caller port issue.
+
+Author : Yanghui Ou
+  Date : Feb 17, 2020
+'''
+from pymtl3 import *
+
+class BoundaryUnit( Component ):
+
+  def construct( s, default_rdy=False ):
+    s.default_rdy = default_rdy
+
+  @non_blocking( lambda s: s.default_rdy )
+  def recv( s, *args, **kwargs ):
+    assert False, "The boundary unit is not supposed to receive any message!"
