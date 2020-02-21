c66101d33dee236ddf9760dbe6b4bdca2e59e0a6
yo96 <yo96@cornell.edu>
2020-02-21 09:58:12 -0500

    remove connect format

--- ocnlib/utils/connects.py
+++ ocnlib/utils/connects.py
@@ -8,69 +8,10 @@ Author : Yanghui Ou
   Date : Jan 29, 2020
 '''
 from pymtl3 import *
-from ..packets.packet_formats import _FIELDS
 from pymtl3.datatypes.bitstructs import (
   is_bitstruct_class, 
   _FIELDS as bitstruct_fields,
 )
-
-#------------------------------------------------------------------------- 
-# _name_mangle
-#------------------------------------------------------------------------- 
-# Flattens a dsl name so that it can be used for creating new signals.
-# TODO: list?
-
-def _name_mangle( dsl_name ):
-  name = dsl_name.replace( '[', '' ).replace( ']', '' )
-  lst = name.split( '.' )[1:] # get rid of s.
-  return '_'.join( lst ) 
-
-#------------------------------------------------------------------------- 
-# connect_format
-#------------------------------------------------------------------------- 
-# Automatically create wires from a packet format and connect them to the
-# corresponding slices.
-
-def connect_format( host, Format, signal ):
-  slice_dict = Format.__dict__[ _FIELDS ]
-  for fname, fslice in slice_dict.items():
-    # Generate a new name
-    new_name = _name_mangle( signal._dsl.full_name ) + f'_{fname}'
-    if hasattr( host, new_name ):
-      raise AttributeError( f'Failed to add a new wire! {host} already has an attribute called {new_name}!' )
-
-    # Create a new wire and add it to host
-    bitwidth = fslice.stop - fslice.start
-    new_wire = Wire( mk_bits( bitwidth ) )
-    print( f'setting {new_name} to {host}' )
-    setattr( host, new_name, new_wire )
-    connect( new_wire, signal[ fslice ] )
-
-#------------------------------------------------------------------------- 
-# connect_union_wire
-#------------------------------------------------------------------------- 
-# Automatically create a bitstruct for a packet format and adds a wire to
-# the component.
-# NOTE: The argument [signal] must has type of Bits.
-# NOTE: We can also create a new level of Component that has this function
-# as a method.
-
-def connect_union( host, Format, wire_name, signal ):
-  slice_dict  = Format.__dict__[ _FIELDS ]
-  field_dict  = { fname : mk_bits( fslice.stop - fslice.start ) for fname, fslice in slice_dict.items() }
-
-  # FIXME: Possible name confilct if two formats in different modules
-  # have the same name.
-  struct_name = f'{Format.__name__}Struct'
-  StructType  = mk_bitstruct( f'', field_dict )
-  new_wire    = Wire( StructType )
-
-  # Add the new wire to host
-  setattr( host, wire_name, new_wire )
-
-  # Connect each field to the corresponding slice
-  for fname, fslice in slice_dict.items():
-    connect( getattr( new_wire, fname ), signal[ fslice ] )
 
 #------------------------------------------------------------------------- 
 # bitstruct_to_slice_h
@@ -102,29 +43,6 @@ def bitstruct_to_slices( cls ):
   slices = []
   _bitstruct_to_slices_h( cls, slices )
   return slices
-
-#------------------------------------------------------------------------- 
-# _connect_bitstruct_h
-#------------------------------------------------------------------------- 
-# TODO: it might be more efficent to just traverse the tree once.
-# TODO: use get_type() API once it is added in pymtl3.
-
-# def _connect_bitstruct_h( field, bits_signal, slices_stack ):
-#   field_type = field._dsl.Type
-# 
-#   # Base case: current field is a BitsN
-#   if not is_bitstruct_class( field_type ):
-#     assert issubclass( field_type, Bits )
-#     connect( field, bits_signal[ slices_stack.pop() ] )
-#     return
-# 
-#   # Recursive case: current field is a bitstruct
-#   else:
-#     fields_dict = getattr( field_type, bitstruct_fields )
-#     for fname, ftype in fields_dict.items():
-#       cur_field = getattr( field, fname )
-#       _connect_bitstruct_h( cur_field, bits_signal, slices_stack )
-#     return
 
 #------------------------------------------------------------------------- 
 # _connect_bitstruct_h
