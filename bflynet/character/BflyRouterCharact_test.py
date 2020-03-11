2c007bc50d228a671e279776a2a655cf470e6c9b
yo96 <yo96@cornell.edu>
2020-03-11 11:32:36 -0400

    use verilog backend

--- bflynet/character/BflyRouterCharact_test.py
+++ bflynet/character/BflyRouterCharact_test.py
@@ -199,4 +199,4 @@ def test_char( k_ary, n_fly ):
   th = TestHarness( PacketType, k_ary, n_fly, pos_row, pos_fly,
                     src_packets, sink_packets, 0, 0, 0, 0 )
 
-  run_sim( th, translation='yosys' )
+  run_sim( th, translation='verilog' )
