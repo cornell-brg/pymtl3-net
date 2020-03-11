b5b4b0f99dfec63ae2fe9009411d7b11d9dcf1c3
yo96 <yo96@cornell.edu>
2020-03-11 10:49:10 -0400

    fix import in mesh router

--- meshnet/test/MeshRouterRTL_test.py
+++ meshnet/test/MeshRouterRTL_test.py
@@ -17,7 +17,6 @@ from ocnlib.ifcs.positions import mk_mesh_pos
 from ocnlib.utils import run_sim
 from ocnlib.test.net_sinks import TestNetSinkRTL
 from pymtl3 import *
-from pymtl3.passes.backends.sverilog import ImportPass, TranslationPass
 from pymtl3.stdlib.test.test_srcs import TestSrcRTL
 from router.InputUnitRTL import InputUnitRTL
 from router.OutputUnitRTL import OutputUnitRTL
