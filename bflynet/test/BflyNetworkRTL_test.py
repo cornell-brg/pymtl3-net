06a178291d8602fd20ea26f6635f2027f66a8015
yo96 <yo96@cornell.edu>
2020-03-11 10:51:54 -0400

    fix import in bfly net

--- bflynet/test/BflyNetworkRTL_test.py
+++ bflynet/test/BflyNetworkRTL_test.py
@@ -12,7 +12,6 @@ from ocnlib.ifcs.positions import *
 from ocnlib.utils import run_sim
 from ocnlib.test.net_sinks import TestNetSinkRTL
 from pymtl3 import *
-from pymtl3.passes.backends.sverilog import ImportPass, TranslationPass
 from pymtl3.stdlib.rtl.queues import NormalQueueRTL
 from pymtl3.stdlib.test import TestVectorSimulator
 from pymtl3.stdlib.test.test_srcs import TestSrcRTL
