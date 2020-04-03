5a8676eb79c20580425173547793cbce625a8342
pp482 <pp482@cornell.edu>
2020-04-03 16:35:21 -0400

    Fix torus tests on CI

--- torusnet/test/TorusNetworkRTL_test.py
+++ torusnet/test/TorusNetworkRTL_test.py
@@ -120,7 +120,7 @@ class TorusNetwork_Tests:
     th = TestHarness( Pkt, ncols, nrows, src_pkts, dst_pkts )
     run_sim( th, cmdline_opts )
 
-  @pytest.mark.skipif('CI' in os.environ)
+  @pytest.mark.skipif('CI' in os.environ, reason='too long on CI')
   def test_simple_5x5( s, cmdline_opts ):
     ncols = 5
     nrows = 5
@@ -136,7 +136,7 @@ class TorusNetwork_Tests:
     th = TestHarness( Pkt, ncols, nrows, src_pkts, dst_pkts )
     run_sim( th, cmdline_opts )
 
-  @pytest.mark.skipif('CI' in os.environ)
+  @pytest.mark.skipif('CI' in os.environ, reason='too long on CI')
   def test_simple_8x8( s, cmdline_opts ):
     ncols = 8
     nrows = 8
@@ -152,7 +152,7 @@ class TorusNetwork_Tests:
     th = TestHarness( Pkt, ncols, nrows, src_pkts, dst_pkts )
     run_sim( th, cmdline_opts )
 
-  @pytest.mark.skipif('CI' in os.environ)
+  @pytest.mark.skipif('CI' in os.environ, reason='too long on CI')
   @hypothesis.settings( deadline=None, max_examples=5 )
   # @hypothesis.reproduce_failure('4.24.4', 'AAMDAQEAAAQAAA==') #(1:0)>(0:4)
   @hypothesis.given(
