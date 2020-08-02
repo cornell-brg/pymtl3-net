a772284311ad89d1c7c66c50eef4ea4e84b87722
yo96 <yo96@cornell.edu>
2020-08-01 22:07:58 -0400

    mesh tile rtl

---
+++ meshnet/physical/MeshTile.py
@@ -0,0 +1,45 @@
+'''
+==========================================================================
+MeshTile.py
+==========================================================================
+A tile module for mesh.
+
+Author : Yanghui Ou
+  Date : July 30, 2020
+'''
+from pymtl3 import *
+from pymtl3.stdlib.ifcs import RecvIfcRTL, SendIfcRTL
+
+from ocnlib.rtl.DummyNode import DummyNode
+
+from ..MeshRouterMflitRTL import MeshRouterMflitRTL
+
+class MeshTile( Component ):
+
+  def construct( s, Header, Position, ):
+
+    # Local parameter
+    
+    s.PhitType = mk_bits( Header.nbits )
+
+    # Interface
+
+    s.recv = [ RecvIfcRTL( s.PhitType ) for _ in range(4) ]
+    s.send = [ SendIfcRTL( s.PhitType ) for _ in range(4) ]
+    s.pos  = InPort( Position )
+
+    # Component
+
+    s.core   = DummyNode( Header )
+    s.router = MeshRouterMflitRTL( Header, Position )
+
+    for i in range(4):
+      s.recv[i] //= s.router.recv[i]
+      s.send[i] //= s.router.send[i]
+
+    s.pos     //= s.router.pos
+    s.router.send[4] //= s.core.recv
+    s.router.recv[4] //= s.core.send
+  
+  def line_trace( s ):
+    return s.router.line_trace()
---
+++ meshnet/physical/test/MeshTile_test.py
@@ -0,0 +1,43 @@
+'''
+==========================================================================
+MeshTile_test.py
+==========================================================================
+Some simple tests for the MeshTile.
+
+Author : Yanghui Ou
+  Date : July 30, 2020
+'''
+from pymtl3 import *
+from pymtl3.passes.backends.verilog import *
+
+from ..MeshTile import MeshTile
+
+@bitstruct
+class Header64:
+  src_x : Bits8
+  src_y : Bits8
+  dst_x : Bits8
+  dst_y : Bits8
+  plen  : Bits4
+  filler: Bits28
+
+@bitstruct
+class Position:
+  pos_x : Bits8
+  pos_y : Bits8
+
+def test_translate():
+  assert Header64.nbits == 64
+  assert Position.nbits == 16
+
+  dut = MeshTile( Header64, Position )
+  dut.set_metadata( VerilogTranslationPass.explicit_module_name, f'MeshTile' )
+  dut.set_metadata( VerilogTranslationImportPass.enable, True )
+  dut.elaborate()
+  dut = VerilogTranslationImportPass()( dut )
+
+  dut.apply( DefaultPassGroup() )
+  dut.sim_reset()
+  dut.sim_tick()
+  dut.sim_tick()
+
