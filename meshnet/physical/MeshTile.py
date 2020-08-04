0939cc9de8ebf29cc4d860f284b06a9d8228e611
yo96 <yo96@cornell.edu>
2020-08-04 12:58:53 -0400

    add pos reg to cut the long In2Out path

--- meshnet/physical/MeshTile.py
+++ meshnet/physical/MeshTile.py
@@ -9,6 +9,7 @@ Author : Yanghui Ou
 '''
 from pymtl3 import *
 from pymtl3.stdlib.ifcs import RecvIfcRTL, SendIfcRTL
+from pymtl3.stdlib.basic_rtl import Reg
 
 from ocnlib.rtl.DummyNode import DummyNode
 
@@ -30,14 +31,17 @@ class MeshTile( Component ):
 
     # Component
 
-    s.core   = DummyNode( Header )
-    s.router = MeshRouterMflitRTL( Header, Position )
+    s.core    = DummyNode( Header )
+    s.router  = MeshRouterMflitRTL( Header, Position )
+    s.pos_reg = Reg( Position )
 
     for i in range(4):
       s.recv[i] //= s.router.recv[i]
       s.send[i] //= s.router.send[i]
 
-    s.pos     //= s.router.pos
+    s.pos //= s.pos_reg.in_
+    s.pos_reg.out //= s.router.pos
+
     s.router.send[4] //= s.core.recv
     s.router.recv[4] //= s.core.send
   
