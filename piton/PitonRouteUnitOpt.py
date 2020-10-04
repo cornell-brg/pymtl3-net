3ff18fa10e43a039b70af3a9fce99e85ad495373
yo96 <yo96@cornell.edu>
2020-10-04 18:59:17 -0400

    pull out routing logic as a separate module

--- piton/PitonRouteUnitOpt.py
+++ piton/PitonRouteUnitOpt.py
@@ -41,15 +41,18 @@ class PitonRouteUnitOpt( Component ):
   # construct
   #-----------------------------------------------------------------------
 
-  def construct( s, PositionType, RoutingLogicType=PitonRoutingLogic, plen_field_name='plen' ):
+  def construct( s, PositionType, RoutingLogicType=PitonRoutingLogic, 
+                 plen_field_name='plen' ):
 
     # Local parameter
 
     assert PitonNoCHeader.nbits == 64
-    s.num_outports = 5
-    s.PhitType     = Bits64
-    s.STATE_HEADER = 0
-    s.STATE_BODY   = 1
+    s.num_outports  = 5
+    s.PhitType      = Bits64
+
+    s.STATE_HEADER       = 0
+    s.STATE_NORMAL_BODY  = 1
+    s.STATE_EXPRESS_BODY = 2
 
     PLenType = Bits8
     XType    = get_field_type( PositionType, 'pos_x' )
@@ -66,8 +69,8 @@ class PitonRouteUnitOpt( Component ):
     # Components
 
     s.header      = Wire( PitonNoCHeader )
-    s.state       = Wire( Bits1 )
-    s.state_next  = Wire( Bits1 )
+    s.state       = Wire( Bits2 )
+    s.state_next  = Wire( Bits2 )
     s.out_dir_r   = Wire( Bits3 )
     s.out_dir     = Wire( Bits3 )
     s.any_give_en = Wire( Bits1 )
@@ -108,15 +111,16 @@ class PitonRouteUnitOpt( Component ):
       else:
         s.state <<= s.state_next
 
+    # TODO:
     @update
     def up_state_next():
       s.state_next @= s.state
       if s.state == s.STATE_HEADER:
         # If the packet has body flits
         if s.any_give_en & ( s.header.plen > 0 ):
-          s.state_next @= s.STATE_BODY
+          s.state_next @= s.STATE_NORMAL_BODY
 
-      else: # STATE_BODY
+      else: # STATE_NORMAL_BODY
         if ( s.counter.count == 1 ) & s.any_give_en:
           s.state_next @= s.STATE_HEADER
 
@@ -132,7 +136,7 @@ class PitonRouteUnitOpt( Component ):
     def up_counter_load():
       s.counter.load @= 0
       if s.state == s.STATE_HEADER:
-        s.counter.load @= ( s.state_next == s.STATE_BODY )
+        s.counter.load @= ( s.state_next == s.STATE_NORMAL_BODY )
 
     @update
     def up_out_dir():
@@ -140,51 +144,6 @@ class PitonRouteUnitOpt( Component ):
 
       if ( s.state == s.STATE_HEADER ) & s.get.rdy:
         s.out_dir @= s.routing_logic.out_dir
-
-    # Routing logic
-
-    # s.offchip //= lambda: s.header.chipid != s.pos.chipid
-
-    # @update
-    # def up_dst():
-    #   s.dst_x @= 0
-    #   s.dst_y @= 0
-    #   if ~s.offchip:
-    #     s.dst_x @= s.header.xpos
-    #     s.dst_y @= s.header.ypos
-
-    # @update
-    # def up_out_dir():
-    #   s.out_dir @= s.out_dir_r
-
-    #   if ( s.state == s.STATE_HEADER ) & s.get.rdy:
-    #     s.out_dir @= 0
-
-    #     # Offchip port
-    #     if ( s.pos.pos_x == 0 ) & ( s.pos.pos_y == 0 ) & s.offchip:
-    #       s.out_dir @= NORTH
-
-    #     elif ( s.dst_x == s.pos.pos_x ) & ( s.dst_y == s.pos.pos_y ):
-    #       # Use fbits to route to final destination
-    #       if s.header.fbits == FBITS_NORTH:
-    #         s.out_dir @= NORTH
-    #       elif s.header.fbits == FBITS_SOUTH:
-    #         s.out_dir @= SOUTH
-    #       elif s.header.fbits == FBITS_WEST:
-    #         s.out_dir @= WEST
-    #       elif s.header.fbits == FBITS_EAST:
-    #         s.out_dir @= EAST
-    #       else:
-    #         s.out_dir @= SELF
-
-    #     elif s.dst_x < s.pos.pos_x:
-    #       s.out_dir @= WEST
-    #     elif s.dst_x > s.pos.pos_x:
-    #       s.out_dir @= EAST
-    #     elif s.dst_y < s.pos.pos_y:
-    #       s.out_dir @= NORTH
-    #     else: # s.dst_y > s.pos.pos_y:
-    #       s.out_dir @= SOUTH
 
     @update_ff
     def up_out_dir_r():
@@ -194,7 +153,7 @@ class PitonRouteUnitOpt( Component ):
     def up_give_rdy_hold():
       for i in range( s.num_outports ):
         s.give[i].rdy @= ( i == s.out_dir ) & s.get.rdy
-        s.hold[i]     @= ( i == s.out_dir ) & ( s.state == s.STATE_BODY )
+        s.hold[i]     @= ( i == s.out_dir ) & ( s.state == s.STATE_NORMAL_BODY )
 
   #-----------------------------------------------------------------------
   # line_trace
@@ -206,7 +165,7 @@ class PitonRouteUnitOpt( Component ):
     pos   = f'<{s.pos.pos_x},{s.pos.pos_y}>'
     count = f'{s.counter.count}'
     state = 'H' if s.state == s.STATE_HEADER else \
-            'B' if s.state == s.STATE_BODY else   \
+            'B' if s.state == s.STATE_NORMAL_BODY else   \
             '!'
     dir   = 'n' if s.out_dir == NORTH else \
             's' if s.out_dir == SOUTH else \
