cc9d136dac4a8750157be4b413619bb7596ec1b6
yo96 <yo96@cornell.edu>
2020-10-05 18:53:25 -0400

    valrdy router that actually uses valrdy internally

--- piton/PitonRouterValRdy.py
+++ piton/PitonRouterValRdy.py
@@ -8,12 +8,16 @@ Author : Yanghui Ou
   Date : Jun 3, 2020
 '''
 from pymtl3 import *
-from pymtl3.stdlib.ifcs import InValRdyIfc, OutValRdyIfc
 from pymtl3.passes.backends.verilog import *
+from pymtl3.stdlib.ifcs import InValRdyIfc, OutValRdyIfc
 
-from ocnlib.ifcs.enrdy_adapters import InValRdy2Send, Recv2OutValRdy
+from ocnlib.rtl.valrdy_queues import NormalQueueRTL
+from router.InputUnitValRdy import InputUnitValRdy
+from router.OutputUnitValRdy import OutputUnitValRdy
+from router.SwitchUnitGrantHoldValRdy import SwitchUnitGrantHoldValRdy
 
-from .PitonRouter import PitonRouter
+from .PitonNoCHeader import PitonNoCHeader
+from .PitonRouteUnitValRdy import PitonRouteUnitValRdy
 
 #-------------------------------------------------------------------------
 # PitonRouterValRdy
@@ -21,7 +25,20 @@ from .PitonRouter import PitonRouter
 
 class PitonRouterValRdy( Component ):
 
-  def construct( s, PositionType ):
+  def construct( s,
+    PositionType,
+    InputUnitType  = InputUnitValRdy,
+    RouteUnitType  = PitonRouteUnitValRdy,
+    SwitchUnitType = SwitchUnitGrantHoldValRdy,
+    OutputUnitType = OutputUnitValRdy,
+  ):
+
+    # Local parameter
+
+    s.num_inports  = 5
+    s.num_outports = 5
+    assert PitonNoCHeader.nbits == 64
+    s.PhitType     = Bits64
 
     # Metadata
 
@@ -33,22 +50,41 @@ class PitonRouterValRdy( Component ):
     s.out = [ OutValRdyIfc( Bits64 ) for _ in range(5) ]
     s.pos = InPort( PositionType )
 
-    # Component
+    # Components
 
-    s.in2send  = [ InValRdy2Send ( Bits64 ) for _ in range(5) ]
-    s.recv2out = [ Recv2OutValRdy( Bits64 ) for _ in range(5) ]
-    s.router   = PitonRouter( PositionType )
+    s.input_units  = [ InputUnitType( s.PhitType, QueueType=NormalQueueRTL )
+                       for _ in range( s.num_inports ) ]
+
+    s.route_units  = [ RouteUnitType( PositionType )
+                       for i in range( s.num_inports ) ]
+
+    s.switch_units = [ SwitchUnitType( s.PhitType, s.num_inports )
+                       for _ in range( s.num_outports ) ]
+
+    s.output_units = [ OutputUnitType( s.PhitType )
+                       for _ in range( s.num_outports ) ]
 
     # Connections
 
-    s.pos //= s.router.pos
+    for i in range( s.num_inports ):
+      s.in_[i]             //= s.input_units[i].in_
+      s.input_units[i].out //= s.route_units[i].in_
+      s.pos                //= s.route_units[i].pos
 
-    for i in range(5):
-      s.in_[i]          //= s.in2send[i].in_
-      s.in2send[i].send //= s.router.recv[i]
-      s.router.send[i]  //= s.recv2out[i].recv
-      s.recv2out[i].out //= s.out[i]
+    for i in range( s.num_inports ):
+      for j in range( s.num_outports ):
+        s.route_units[i].out[j] //= s.switch_units[j].in_[i]
+        s.route_units[i].hold[j] //= s.switch_units[j].hold[i]
+
+    for j in range( s.num_outports ):
+      s.switch_units[j].out //= s.output_units[j].in_
+      s.output_units[j].out //= s.out[j]
+
+  #-----------------------------------------------------------------------
+  # Line trace
+  #-----------------------------------------------------------------------
 
   def line_trace( s ):
-    return s.router.line_trace()
-
+    in_trace  = '|'.join([ f'{ifc}' for ifc in s.in_ ])
+    out_trace = '|'.join([ f'{ifc}' for ifc in s.out ])
+    return f'{in_trace}({s.pos}){out_trace}'
