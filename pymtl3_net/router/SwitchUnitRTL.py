c63908ae7b4282dae08bb8cb5c6678f0dcb938c2
yo96 <yo96@cornell.edu>
2022-01-13 22:45:21 -0500

    change switch unit to val/rdy

--- pymtl3_net/router/SwitchUnitRTL.py
+++ pymtl3_net/router/SwitchUnitRTL.py
@@ -8,7 +8,7 @@ Author : Yanghui Ou, Cheng Tan
   Date : Feb 28, 2019
 """
 from pymtl3 import *
-from pymtl3.stdlib.ifcs import GetIfcRTL, GiveIfcRTL, SendIfcRTL
+from pymtl3.stdlib.stream.ifcs import SendIfcRTL, RecvIfcRTL
 from pymtl3.stdlib.basic_rtl import Mux
 from pymtl3.stdlib.basic_rtl import RoundRobinArbiterEn
 from pymtl3.stdlib.basic_rtl import Encoder
@@ -22,25 +22,19 @@ class SwitchUnitRTL( Component ):
 
     s.num_inports = num_inports
     s.sel_width   = clog2( num_inports )
-    s.set_ocp = 0
-    s.clear_ocp = 0
 
     # Interface
 
-    s.get  = [ GetIfcRTL( PacketType ) for _ in range( s.num_inports ) ]
-    s.give = GiveIfcRTL( PacketType )
-    s.out_ocp = OutPort()
+    s.recv = [ RecvIfcRTL( PacketType ) for _ in range( s.num_inports ) ]
+    s.send = SendIfcRTL( PacketType )
 
     # Components
-
-    s.get_en  = [ Wire() for _ in range( s.num_inports ) ]
-    s.get_rdy = [ Wire() for _ in range( s.num_inports ) ]
 
     s.arbiter = RoundRobinArbiterEn( num_inports )
     s.arbiter.en //= 1
 
     s.mux = Mux( PacketType, num_inports )
-    s.mux.out //= s.give.ret
+    s.mux.out //= s.send.msg
 
     s.encoder = Encoder( num_inports, s.sel_width )
     s.encoder.in_ //= s.arbiter.grants
@@ -49,22 +43,22 @@ class SwitchUnitRTL( Component ):
     # Connections
 
     for i in range( num_inports ):
-      s.get[i].rdy //= s.arbiter.reqs[i]
-      s.get[i].ret //= s.mux.in_[i]
-      s.get[i].en  //= s.get_en[i]
-      s.get[i].rdy //= s.get_rdy[i]
+      s.recv[i].val //= s.arbiter.reqs[i]
+      s.recv[i].msg //= s.mux.in_[i]
 
     @update
-    def up_give():
-      s.give.rdy @= s.arbiter.grants > 0
+    def up_send_val():
+      s.send.val @= s.arbiter.grants > 0
 
+    # TODO: assert at most one rdy bit
     @update
     def up_get_en():
       for i in range( num_inports ):
-        s.get_en[i] @= s.give.en & ( s.mux.sel == i )
+        s.recv[i].rdy @= s.send.rdy & ( s.mux.sel == i )
 
   # Line trace
 
   def line_trace( s ):
-    in_trace = [ str(s.get[i]) for i in range( s.num_inports ) ]
-    return "{}(){}".format( "|".join( in_trace ), s.give )
+    in_trace = [ str(s.recv[i]) for i in range( s.num_inports ) ]
+    return "{}(){}".format( "|".join( in_trace ), s.send )
+
--- pymtl3_net/router/test/SwitchUnitRTL_test.py
+++ pymtl3_net/router/test/SwitchUnitRTL_test.py
@@ -21,23 +21,24 @@ from pymtl3_net.ocnlib.ifcs.packets import mk_generic_pkt
 def test_switch_unit_simple():
   dut = SwitchUnitRTL( Bits32, num_inports=5 )
   dut.elaborate()
-  dut.apply( DefaultPassGroup() )
+  dut.apply( DefaultPassGroup(linetrace=True) )
   dut.sim_reset()
 
-  dut.get[0].rdy @= 1
-  dut.get[0].ret @= 0xfaceb00c
-  dut.get[1].rdy @= 1
-  dut.get[1].ret @= 0xdeadface
-  dut.get[4].rdy @= 1
-  dut.get[4].ret @= 0xbaadbeef
-  dut.give.en @= 0
+  dut.recv[0].val @= 1
+  dut.recv[0].msg @= 0xfaceb00c
+  dut.recv[1].val @= 1
+  dut.recv[1].msg @= 0xdeadface
+  dut.recv[4].val @= 1
+  dut.recv[4].msg @= 0xbaadbeef
+  dut.send.rdy@= 0
   dut.sim_eval_combinational()
   dut.sim_tick()
 
   for i in range( 3 ):
-    dut.give.en @= 1
+    dut.send.rdy @= 1
     dut.sim_eval_combinational()
 
-    assert dut.give.rdy
-    assert dut.give.ret in { b32(0xfaceb00c), b32(0xdeadface), b32(0xbaadbeef)  }
+    assert dut.send.rdy
+    assert dut.send.msg in { b32(0xfaceb00c), b32(0xdeadface), b32(0xbaadbeef)  }
     dut.sim_tick()
+
