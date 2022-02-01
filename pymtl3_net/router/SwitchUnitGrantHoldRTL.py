2e4b9b3ce9faae3f60583a35fae3002074bf7e2c
yo96 <yo96@cornell.edu>
2022-02-01 11:49:18 -0500

    change grant-hold switch unit to val/rdy

--- pymtl3_net/router/SwitchUnitGrantHoldRTL.py
+++ pymtl3_net/router/SwitchUnitGrantHoldRTL.py
@@ -11,7 +11,8 @@ Author : Yanghui Ou
 '''
 from pymtl3 import *
 from pymtl3.stdlib.basic_rtl import Mux, Encoder
-from pymtl3.stdlib.ifcs import GetIfcRTL, GiveIfcRTL, SendIfcRTL
+# from pymtl3.stdlib.ifcs import GetIfcRTL, GiveIfcRTL, SendIfcRTL
+from pymtl3.stdlib.stream.ifcs import RecvIfcRTL, SendIfcRTL
 from pymtl3_net.ocnlib.rtl import Counter, GrantHoldArbiter
 
 class SwitchUnitGrantHoldRTL( Component ):
@@ -24,20 +25,20 @@ class SwitchUnitGrantHoldRTL( Component ):
     s.sel_width   = clog2( num_inports )
 
     # Interface
-    s.get  = [ GetIfcRTL( s.Type ) for _ in range( num_inports )  ]
+    s.recv = [ RecvIfcRTL( s.Type ) for _ in range( num_inports )  ]
     s.hold = InPort(num_inports)
-    s.give = GiveIfcRTL( s.Type )
+    s.send = SendIfcRTL( s.Type )
 
     # Components
-    s.granted_get_rdy = Wire()
+    s.granted_recv_val = Wire()
     s.any_hold        = Wire()
 
     s.arbiter = GrantHoldArbiter( nreqs=num_inports )
     s.arbiter.hold //= s.any_hold
-    s.arbiter.en   //= lambda: ~s.any_hold & s.give.en
+    s.arbiter.en   //= lambda: ~s.any_hold & s.send.val & s.send.rdy
 
     s.mux = Mux( s.Type, num_inports )
-    s.mux.out //= s.give.ret
+    s.mux.out //= s.send.msg
 
     s.encoder = Encoder( num_inports, s.sel_width )
     s.encoder.in_  //= s.arbiter.grants
@@ -50,23 +51,23 @@ class SwitchUnitGrantHoldRTL( Component ):
 
     @update
     def up_granted_get_rdy():
-      s.granted_get_rdy @= 0
+      s.granted_recv_val @= 0
       for i in range( num_inports ):
         if s.arbiter.grants[i]:
-          s.granted_get_rdy @= s.get[i].rdy
+          s.granted_recv_val @= s.recv[i].val
 
     for i in range( num_inports ):
-      s.get[i].rdy //= s.arbiter.reqs[i]
-      s.get[i].ret //= s.mux.in_[i]
+      s.recv[i].val //= s.arbiter.reqs[i]
+      s.recv[i].msg //= s.mux.in_[i]
 
     for i in range( num_inports ):
-      s.get[i].en //= lambda: s.give.en & ( s.mux.sel == i )
+      s.recv[i].rdy //= lambda: s.send.rdy & ( s.mux.sel == i )
 
-    s.give.rdy //= s.granted_get_rdy
+    s.send.val //= s.granted_recv_val
 
   def line_trace( s ):
-    in_trace  = '|'.join( [ str(p) for p in s.get ] )
+    in_trace  = '|'.join( [ str(p) for p in s.recv ] )
     hold      = ''.join([ '^' if h else '.' for h in s.hold ])
-    out_trace = f'{s.give}'
+    out_trace = f'{s.send}'
     return f'{in_trace}({hold}){out_trace}'
 
